4560db03fbe34413b24feb062afb0b90c87ba0e4
fchoi1 <fchoi@outlook.com>
2023-01-21 01:49:26 -0500

    completed day 8

--- day8/day8.py
+++ day8/day8.py
@@ -1,22 +1,74 @@
-def getInput():
-    file1 = open('input.txt', 'r')
-    Lines = file1.readlines()
-    data = []
-    # Get input data
-    for line in Lines:
-        data.append(line.strip())
-    return data
 
-def main():
-    """ Main entry point of the app """
-    inputData = getInput()
-
-
+class Trees():
     
-    print('day 1 part 1:', maxCalories[0])
-    print('day 1 part 2:', sum(maxCalories))
-    return maxCalories
+    def getInput(self):
+        file1 = open('input.txt', 'r')
+        #file1 = open('input-test.txt', 'r')
+        Lines = file1.readlines()
+        data = []
+        # Get input data
+        for line in Lines:
+            data.append(line.strip())
+        return data
+
+    def __init__(self) -> None:
+        """ Main entry point of the app """
+        self.inputData = self.getInput()
+        self.trees = []
+        self.numVisible = 0
+        self.size = [0,0] #  [row, col]
+        self.maxScenicScore = 0
+        self.generateGrid()
+
+    def generateGrid(self):
+        for row in self.inputData:
+            self.trees.append([int(x) for x in row])
+
+        self.size[0] = len(self.trees)
+        self.size[1] = len(self.trees[0])
+    
+    def getTreeProps(self, index: int, row: list[int]):
+        left = right = index
+        leftVisible = rightVisible = True
+        leftScore = rightScore = 0
+
+        while left > 0:
+            left  -= 1
+            leftScore += 1
+            if row[index] <= row[left]:
+                leftVisible = False
+                break
+            
+        while right < len(row) - 1:
+            right += 1
+            rightScore += 1
+            if row[index] <= row[right]:
+                rightVisible = False
+                break
+    
+        return  { "scenicScore": leftScore  * rightScore, "isVisible": (leftVisible or rightVisible) }
+
+    def calculateNumVisible(self):
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                treeRowProps = self.getTreeProps(i, [row[j] for row in self.trees])
+                treeColProps = self.getTreeProps(j, self.trees[i])
+                scenicScore = treeColProps['scenicScore'] * treeRowProps['scenicScore']
+                if  treeRowProps['isVisible'] or treeColProps['isVisible']:
+                    self.numVisible += 1
+                if scenicScore > self.maxScenicScore:
+                    self.maxScenicScore = scenicScore
+
+    def getNumVisible(self):
+        return  self.numVisible
+    
+    def getScenicScore(self):
+        return self.maxScenicScore
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
-    main()
+    tree = Trees()
+    tree.calculateNumVisible()
+    print('Day 8 part 1:', tree.getNumVisible())
+    print('Day 8 part 2:', tree.getScenicScore())
+
