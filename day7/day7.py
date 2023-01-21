6e9cfbeb50f104b0d89c05c258f2a3c36bd7f408
fchoi1 <fchoi@outlook.com>
2023-01-20 23:06:43 -0500

    completed day7

--- day7/day7.py
+++ day7/day7.py
@@ -2,8 +2,8 @@
 class Folder():
     
     def getInput(self):
-        file1 = open('input-test.txt', 'r')
-        # file1 = open('input-test.txt', 'r')
+        file1 = open('input.txt', 'r')
+        #file1 = open('input-test.txt', 'r')
         Lines = file1.readlines()
         data = []
         # Get input data
@@ -14,10 +14,17 @@ class Folder():
     def __init__(self) -> None:
         """ Main entry point of the app """
         self.inputData = self.getInput()
-
         self.folder = {}
         self.filePath = []
         self.currentFolder = self.folder
+        self.sizeSum = 0
+
+        self.totalSize = 70000000
+        self.buildFolder()
+        self.currentFolderSize = self.getFolderSize(self.folder)
+        self.deleteSize = 30000000 - (self.totalSize - self.currentFolderSize)
+        self.folderToDelete = None
+        self.folderToDeleteSize = float('inf')
 
     def buildFolder(self) -> int:
         for line in self.inputData:
@@ -26,18 +33,45 @@ class Folder():
             if arguments[0] == '$':
                 if arguments[1] == 'cd':
                     self.updateFilePath(arguments[2])
-            elif arguments[0] == 'dir' and self.filePath[-1] not in  self.currentFolder:
+            elif arguments[0] == 'dir' and self.filePath[-1] not in self.currentFolder:
                 self.currentFolder[arguments[1]] = {}
-            else:
+            elif arguments[0].isdigit():
                 self.currentFolder[arguments[1]] = int(arguments[0])
+
+        #print(self.folder)
+        pass
+
+    def getFolderSize(self, folder) -> int:
+        size = 0
+        for item in folder:
+            if isinstance(folder[item], int):
+                size += folder[item]
+            else:
+                size += self.getFolderSize(folder[item])
+        return size
+
+    def calculateSizeSum(self, folder,  maxSize: int=100000) -> int:
+        for item in folder:
+            if not isinstance(folder[item], int):
+                self.calculateSizeSum(folder[item])
+                folderSize = self.getFolderSize(folder[item])
+
+                if folderSize < maxSize:
+                    self.sizeSum += folderSize
+
+                if folderSize > self.deleteSize and folderSize < self.folderToDeleteSize:
+                    self.folderToDelete = item
+                    self.folderToDeleteSize = folderSize
+        pass
+
+    def getFoldertoDelete(self):
+        #print(self.folderToDelete, 'size', self.folderToDeleteSize)
+        return self.folderToDeleteSize
         
-        print(self.folder)
-        pass
-
-    def getSize(self) -> int:
-        self.buildFolder()
-        pass
-
+    def getSizeSum(self):
+        self.calculateSizeSum(self.folder)
+        return self.sizeSum
+    
     def updateFilePath(self, cmd: str):
         if cmd == '..':
             self.filePath.pop()
@@ -53,4 +87,5 @@ class Folder():
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     folder = Folder()
-    print(folder.getSize())
+    print('Day 7 part 1:', folder.getSizeSum())
+    print('Day 7 part 2:', folder.getFoldertoDelete())
