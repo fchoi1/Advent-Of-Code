6a375e8e568de8fdf695f888b8d17e37bcb33891
fchoi1 <fchoi@outlook.com>
2023-09-11 12:42:37 -0400

    Done day 9

--- 2020/day7/day7.py
+++ 2020/day7/day7.py
@@ -1,6 +1,7 @@
 from typing import Optional, Dict, List, Set
 import re
 
+
 class Haversacks:
     def getInput(self) -> Dict[int, List[str]]:
         inputFile = "input-test.txt" if self.useTest else "input.txt"
@@ -8,27 +9,28 @@ class Haversacks:
             data = {}
             delimiters = "|".join(map(re.escape, [" bags contain ", " ", ", ", "."]))
             for line in file1.readlines():
-                line = line.strip().replace(" bags","").replace(" bag", "")
+                line = line.strip().replace(" bags", "").replace(" bag", "")
                 splited = re.split(delimiters, line)
                 connections = []
-                if splited[3] == 'no': continue
-                for i in range(3, 3+len(splited[4:]), 3):
-                    connections.append((int(splited[i]), splited[i+1]+splited[i+2]))
+                if splited[3] == "no":
+                    continue
+                for i in range(3, 3 + len(splited[4:]), 3):
+                    connections.append((int(splited[i]), splited[i + 1] + splited[i + 2]))
                 data["".join(splited[:2])] = connections
             return data
 
     def __init__(self, useTest: Optional[bool] = False) -> None:
         self.useTest = useTest
         self.bags = self.getInput()
-        self.target = 'shinygold'
+        self.target = "shinygold"
 
     def hasTarget(self, bag: str, visited: Set) -> bool:
         if bag not in self.bags or bag in visited:
             return False
-        
+
         if bag == self.target:
-            return True 
-  
+            return True
+
         visited.add(bag)
 
         total = []
@@ -41,26 +43,26 @@ class Haversacks:
         count = 0
         for bag in self.bags:
             if self.hasTarget(bag, set()) and bag != self.target:
-                count += 1  
+                count += 1
         return count
-    
+
     def getBagsInTarget(self) -> int:
-        return  self.getNumBags(self.target, {})
-     
+        return self.getNumBags(self.target, {})
+
     def getNumBags(self, bag: str, bagDict: Dict) -> int:
-        
         if bag not in self.bags:
             return 0
-        
+
         if bag in bagDict:
             return bagDict[bag]
-        
+
         total = 0
         for num, nextBag in self.bags[bag]:
             total += num + num * self.getNumBags(nextBag, bagDict)
 
         bagDict[bag] = total
-        return  total
+        return total
+
 
 if __name__ == "__main__":
     haversacks = Haversacks()
--- 2020/day8/day8.py
+++ 2020/day8/day8.py
@@ -1,22 +1,73 @@
-from typing import Optional, List
-import re
+from typing import Optional, List, Tuple
 
-class Haversacks:
-    def getInput(self) ->  List:
-        inputFile = "input-test2.txt" if self.useTest else "input.txt"
+
+class Console:
+    def getInput(self) -> List:
+        inputFile = "input-test.txt" if self.useTest else "input.txt"
         with open(inputFile, "r") as file1:
-            data = {}
-            delimiters = "|".join(map(re.escape, [" bags contain ", " ", ", ", "."]))
+            data = []
             for line in file1.readlines():
-                line = line.strip()
+                line = line.strip().split(" ")
+                data.append((line[0], int(line[1])))
 
             return data
 
     def __init__(self, useTest: Optional[bool] = False) -> None:
         self.useTest = useTest
-        self.bags = self.getInput()
+        self.console = self.getInput()
+        self.length = len(self.console)
+        self.visited = set()
+
+    def getAcc(self) -> int:
+        start = 0
+        self.visited = set()
+        acc = 0
+        while start not in self.visited and start < self.length:
+            command, val = self.console[start]
+            self.visited.add(start)
+            if command == "jmp":
+                start = (start + val) % self.length
+                continue
+            if command == "acc":
+                acc += val
+            start += 1
+        return acc
+
+    def isLoop(self, index: int) -> bool:
+        while index not in self.visited and index < self.length:
+            command, val = self.console[index]
+            if command == "jmp":
+                index = index + val
+                continue
+            index += 1
+        return index < self.length
+
+    def fixProgram(self) -> int:
+        index = -1
+        correct = ""
+
+        for n in self.visited:
+            command, val = self.console[n]
+            if command == "jmp":
+                if not self.isLoop(n + 1):
+                    correct = "nop"
+                    index = n
+                    break
+
+            elif command == "nop":
+                if not self.isLoop(n + val):
+                    correct = "jmp"
+                    index = n
+                    break
+
+        if index == -1:
+            return -1
+        self.console[index] = (correct, val)
+
+        return self.getAcc()
+
 
 if __name__ == "__main__":
-    haversacks = Haversacks()
-    print("Day 8 part 1:")
-    print("Day 8 part 2:")
+    console = Console()
+    print("Day 8 part 1:", console.getAcc())
+    print("Day 8 part 2:", console.fixProgram())
