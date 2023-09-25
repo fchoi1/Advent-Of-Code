a5cbb7c195e9698a27bb258d4e5d00eaf369692d
fchoi1 <fchoi@outlook.com>
2023-09-25 17:33:28 -0400

    added day 12 of 2015

--- 2015/day12/day12.js
+++ 2015/day12/day12.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 class Document {
   getInput() {
     const inputFile = this.useTest ? "input-test.txt" : "input.txt";
-    const locationMap = {};
+
     try {
       return fs.readFileSync(inputFile, "utf8").trim().split("\r\n")[0];
     } catch (err) {
@@ -13,14 +13,50 @@ class Document {
 
   constructor(useTest = false) {
     this.useTest = useTest;
-    this.password = this.getInput();
+    this.json = this.getInput();
+    this.delimiters = / |,|:|\[|\]|\{|\}/;
+    console.log();
   }
 
-  getSum() {
-    return;
+  getSum(string) {
+    return string.split(this.delimiters).reduce((sum, val) => (val && !isNaN(val) ? sum + parseInt(val) : sum), 0);
+  }
+
+  getStringSum() {
+    return this.getSum(this.json);
+  }
+
+  getNonRedCount() {
+    const splitted = this.json.replace(/([\[\]{}])/g, " $1 ").split(/\s+/);
+    const stack = [];
+    let currSum = 0;
+    let hasRed = false;
+    for (const currString of splitted) {
+      if (!currString) continue;
+      if ("{[".includes(currString)) {
+        stack.push([currSum, hasRed]);
+        currSum = 0;
+      } else if ("]}".includes(currString)) {
+        if (stack.length > 1) {
+          let tempSum;
+          [tempSum, hasRed] = stack.pop();
+          currSum += tempSum;
+        }
+      } else {
+        if (hasRed) continue;
+
+        if (currString.includes(':"red')) {
+          hasRed = true;
+          currSum = 0;
+        } else {
+          currSum += this.getSum(currString);
+        }
+      }
+    }
+    return currSum;
   }
 }
 
 const document = new Document();
-console.log("Day 11 part 1:", document.getSum());
-console.log("Day 11 part 2:");
+console.log("Day 11 part 1:", document.getStringSum());
+console.log("Day 11 part 2:", document.getNonRedCount());
