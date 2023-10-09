9ccc5df7e01c6783c842b31a7a64fc14c8eae249
fchoi1 <fchoi@outlook.com>
2023-10-08 22:08:16 -0400

    done day9 of 2016

--- 2016/day9/day9.go
+++ 2016/day9/day9.go
@@ -9,9 +9,10 @@ import (
 )
 
 type File struct {
-	UseTest    bool
-	file       string
-	compressed string
+	UseTest     bool
+	file        string
+	compressed  string
+	compressed2 string
 }
 
 func parseMarker(text string) (int, int) {
@@ -20,8 +21,6 @@ func parseMarker(text string) (int, int) {
 	})
 	num1, _ := strconv.Atoi(splitted[0])
 	num2, _ := strconv.Atoi(splitted[1])
-	fmt.Println(text)
-
 	return num1, num2
 }
 
@@ -40,6 +39,34 @@ func (this *File) getInput() {
 	}
 	defer file.Close()
 }
+func getCompressedLength(text string, times int) int {
+
+	var str string
+	markerFound := false
+	var markStart int
+	var skipIndex int
+	length := 0
+	for i, char := range text {
+
+		if i < skipIndex {
+			continue
+		}
+		if !markerFound && char == '(' {
+			markerFound = true
+			markStart = i
+		} else if !markerFound {
+			str += string(char)
+			length++
+		}
+		if markerFound && char == ')' {
+			markerFound = false
+			strLen, times := parseMarker(text[markStart:i])
+			skipIndex = i + strLen + 1
+			length += getCompressedLength(text[i+1:skipIndex], times)
+		}
+	}
+	return length * times
+}
 
 func (this *File) compressFile() {
 	markerFound := false
@@ -47,15 +74,12 @@ func (this *File) compressFile() {
 	var skipIndex int
 	for i, char := range this.file {
 
-		if i < skipIndex{
-			fmt.Println("skip", i, string(char))
+		if i < skipIndex {
 			continue
 		}
-
 		if !markerFound && char == '(' {
 			markerFound = true
 			markStart = i
-			fmt.Println("found marker", i)
 		} else if !markerFound {
 			this.compressed += string(char)
 		}
@@ -70,16 +94,19 @@ func (this *File) compressFile() {
 }
 
 func (this *File) getFileLength() int {
+	this.compressFile()
 	return len(this.compressed)
 }
 
-func main() {
+func (this *File) getFileLength2() int {
+	return getCompressedLength(this.file, 1)
+}
 
+func main() {
 	file := &File{
 		UseTest: false,
 	}
 	file.getInput()
-	file.compressFile()
 	fmt.Println("Day 8 part 1:", file.getFileLength())
-	fmt.Println("Day 8 part 2:")
+	fmt.Println("Day 8 part 2:", file.getFileLength2())
 }
