ead4c637fd22f77bc3b84a88b82ca4e029516144
fchoi1 <fchoi@outlook.com>
2023-10-16 20:53:50 -0400

    done day 14 of 2016

--- 2016/day14/day14.go
+++ 2016/day14/day14.go
@@ -5,8 +5,12 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"math"
 	"os"
+	"reflect"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 type Pad struct {
@@ -18,7 +22,23 @@ func hashMD5(input string) string {
 	hash := md5.Sum([]byte(input))
 	return hex.EncodeToString(hash[:])
 }
-
+func removeIndexes(slice interface{}, indexes []int) interface{} {
+	v := reflect.ValueOf(slice)
+	if v.Kind() != reflect.Slice {
+		return nil
+	}
+	indexSet := make(map[int]bool)
+	for _, idx := range indexes {
+		indexSet[idx] = true
+	}
+	result := reflect.MakeSlice(v.Type(), 0, 0)
+	for i := 0; i < v.Len(); i++ {
+		if !indexSet[i] {
+			result = reflect.Append(result, v.Index(i))
+		}
+	}
+	return result.Interface()
+}
 func (this *Pad) getInput() {
 	inputFile := "input.txt"
 	if this.UseTest {
@@ -32,19 +52,52 @@ func (this *Pad) getInput() {
 	defer file.Close()
 }
 
-func (this *Pad) getIndex() int {
-	num := 0
-	keyList := [][]string{}
-	targetKey = 64
-	var hashStr string
-	for len(this.Password) < 8 || !isArrayFull(password2Arr) {
-		hashStr = this.Salt + strconv.Itoa(num)
-		hashed = hashMD5(hashStr)
-		fmt.Println(hashed)
-		num++
+func (this *Pad) getIndex(isPart2 bool) int {
+	foundIndex := math.MaxInt64
+	keyIndex := []int{}
+	charList := []string{}
+	keyList := []int{}
+	var char2Add string
+	for num := 0; num < foundIndex; num++ {
+		has3 := false
+		for len(keyIndex) > 0 && num-keyIndex[0] > 1000 {
+			keyIndex = keyIndex[1:]
+			charList = charList[1:]
+		}
+		hashed := hashMD5(this.Salt + strconv.Itoa(num))
+		if isPart2 {
+			for i := 0; i < 2016; i++ {
+				hashed = hashMD5(hashed)
+			}
+		}
+		indexToRemove := []int{}
+		for i, char := range hashed {
+			if i+2 < 32 && hashed[i:i+3] == strings.Repeat(string(char), 3) && !has3 {
+				char2Add = string(char)
+				has3 = true
+			}
+			if i+4 < 32 && hashed[i:i+5] == strings.Repeat(string(char), 5) {
+				for i, char2Match := range charList {
+					if string(char) == char2Match {
+						indexToRemove = append(indexToRemove, i)
+						keyList = append(keyList, keyIndex[i])
+						if len(keyList) == 64 {
+							foundIndex = num + 5000
+						}
+					}
+				}
+				break // only first instance of 5?
+			}
+		}
+		keyIndex = removeIndexes(keyIndex, indexToRemove).([]int)
+		charList = removeIndexes(charList, indexToRemove).([]string)
+		if has3 {
+			keyIndex = append(keyIndex, num)
+			charList = append(charList, char2Add)
+		}
 	}
-
-	return 0
+	sort.Ints(keyList)
+	return keyList[63]
 }
 
 func main() {
@@ -52,6 +105,7 @@ func main() {
 		UseTest: false,
 	}
 	pad.getInput()
-	fmt.Println("Day 14 part 1:", pad.getIndex())
-	// fmt.Println("Day 14 part 2:", pad.getMaxLocations())
+	fmt.Println("Day 14 part 1:", pad.getIndex(false))
+	fmt.Println("Day 14 part 2:", pad.getIndex(true))
+	//Total Runtime ~ 8.8s
 }
