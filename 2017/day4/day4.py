791859c33d46623983bdfa9f94921f96b4c7b748
fchoi1 <fchoi@outlook.com>
2023-10-26 16:21:56 -0400

    done day 4 of 2017

---
+++ 2017/day4/day4.py
@@ -0,0 +1,37 @@
+from typing import List, Optional, Tuple
+
+
+class Passphrase:
+    def getInput(self) -> List[List[str]]:
+        inputFile = "input-test.txt" if self.useTest else "input.txt"
+        with open(inputFile, "r") as file1:
+            return [line.strip().split(" ") for line in file1.readlines()]
+
+    def __init__(self, useTest: Optional[bool] = False) -> None:
+        self.useTest = useTest
+        self.stringList = self.getInput()
+
+    def getValid(self, isPart2: Optional[bool] = False) -> int:
+        count = 0
+        for strings in self.stringList:
+            wordDict = set()
+            for word in strings:
+                key = self.generateKey(word) if isPart2 else word
+                if key in wordDict:
+                    break
+                wordDict.add(key)
+            else:
+                count += 1
+        return count
+
+    def generateKey(self, word: str) -> Tuple[str]:
+        charList = [0] * 26
+        for char in word:
+            charList[ord(char) - ord("a")] += 1
+        return tuple(charList)
+
+
+if __name__ == "__main__":
+    passphrase = Passphrase()
+    print("Day 4 part 1:", passphrase.getValid())
+    print("Day 4 part 2:", passphrase.getValid(True))
