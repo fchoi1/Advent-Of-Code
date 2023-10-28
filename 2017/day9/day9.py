1e22c0320719d2e0fd6c69ec86acdd72ccc54d29
fchoi1 <fchoi@outlook.com>
2023-10-27 23:02:19 -0400

    done day 9 of 2017

---
+++ 2017/day9/day9.py
@@ -0,0 +1,50 @@
+from typing import List, Optional, Tuple, Union, Dict
+from collections import deque
+
+
+class Stream:
+    def getInput(self) -> str:
+        inputFile = "input-test.txt" if self.useTest else "input.txt"
+
+        with open(inputFile, "r") as file1:
+            return file1.readline().strip()
+
+    def __init__(self, useTest: Optional[bool] = False) -> None:
+        self.useTest = useTest
+        self.stream = self.getInput()
+        self.score = self.garbage = 0
+        self.calculateStream(0, 0)
+
+    def calculateStream(self, index: int, level: int) -> Tuple[int]:
+        garbage = False
+        while index < len(self.stream):
+            sym = self.stream[index]
+            if not garbage:
+                if sym == "<":
+                    garbage = True
+                elif sym == "{":
+                    score, index = self.calculateStream(index + 1, level + 1)
+                    self.score += score
+                elif sym == "}":
+                    return level, index
+            else:
+                self.garbage += 1
+                if sym == ">":
+                    garbage = False
+                    self.garbage -= 1
+                elif sym == "!":
+                    self.garbage -= 1
+                    index += 1
+            index += 1
+
+    def getScore(self) -> int:
+        return self.score
+
+    def getGarbage(self) -> int:
+        return self.garbage
+
+
+if __name__ == "__main__":
+    stream = Stream()
+    print("Day 9 part 1:", stream.getScore())
+    print("Day 9 part 2:", stream.getGarbage())
