a613781df4054165b0ce630e58b5b617c7d66b9c
fchoi1 <fchoi@outlook.com>
2023-10-29 14:47:40 -0400

    done day 10 part 1

--- 2017/day10/day10.py
+++ 2017/day10/day10.py
@@ -14,45 +14,30 @@ class Knot:
         self.hash = [i for i in range(self.length)]
         self.firstTwo = 0
         self.runLengths()
-        print(self.lengths, self.hash)
 
     def reverseSubset(self, start: int, end: int):
         if end > self.length:
             end %= self.length
-            tempHash = self.hash[start:] + self.hash[:end]
-            tempHash.reverse()
-            i = 0
-            while start != end:
-                self.hash[end], self.hash[start] = tempHash[i], tempHash[-i-1]
-                print("reverse", tempHash, start, end, i, tempHash[i], tempHash[-1-i])
-                i += 1
-                end -= 1
-                start += 1
-                if end < 0:
-                    end = self.length - 1
-                if start >= self.length:
-                    start = 0
+            sublist = list(reversed(self.hash[start:] + self.hash[:end]))
+            self.hash[start:] = sublist[: len(self.hash) - start]
+            self.hash[:end] = sublist[len(self.hash) - start :]
         else:
-            self.hash[start:end] = list(reversed(self.hash[start:end]))
+            self.hash[start:end] = reversed(self.hash[start:end])
 
     def runLengths(self) -> None:
-        skip = 0
-        index = 0
+        skip = index = 0
         for l in self.lengths:
             subset = index + l
-            print(self.hash, index, subset)
-
             self.reverseSubset(index, subset)
-            index = (index + subset + skip) % self.length
+            index = (subset + skip) % self.length
             skip += 1
-        print("done", self.hash[0], self.hash[1], self.hash)
-        self.firstTwo = self.hash[0] * self.hash[0]
+        self.firstTwo = self.hash[0] * self.hash[1]
 
     def getFirstTwo(self) -> int:
         return self.firstTwo
 
 
 if __name__ == "__main__":
-    knot = Knot(True)
+    knot = Knot()
     print("Day 10 part 1:", knot.getFirstTwo())
     # print("Day 10 part 2:", knot.getGarbage())
