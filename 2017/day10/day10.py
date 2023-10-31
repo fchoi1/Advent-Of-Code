e6628063086eab8c1d08ce8a258faaf91459da06
fchoi1 <fchoi@outlook.com>
2023-10-30 20:30:40 -0400

    done day 10 of 2017

--- 2017/day10/day10.py
+++ 2017/day10/day10.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from functools import reduce
 
 
 class Knot:
@@ -10,10 +11,19 @@ class Knot:
     def __init__(self, useTest: Optional[bool] = False) -> None:
         self.useTest = useTest
         self.lengths = self.getInput()
+        self.newLengths = self.getNewLengths()
         self.length = 5 if useTest else 256
-        self.hash = [i for i in range(self.length)]
         self.firstTwo = 0
-        self.runLengths()
+
+    def reset(self) -> None:
+        self.hash = [i for i in range(self.length)]
+        self.skip = self.index = 0
+
+    def getNewLengths(self) -> List[int]:
+        string = ",".join(map(str, self.lengths))
+        lengths = [ord(char) for char in string]
+        lengths += [17, 31, 73, 47, 23]
+        return lengths
 
     def reverseSubset(self, start: int, end: int):
         if end > self.length:
@@ -24,20 +34,34 @@ class Knot:
         else:
             self.hash[start:end] = reversed(self.hash[start:end])
 
-    def runLengths(self) -> None:
-        skip = index = 0
-        for l in self.lengths:
-            subset = index + l
-            self.reverseSubset(index, subset)
-            index = (subset + skip) % self.length
-            skip += 1
+    def runLengths(self, lengths: List[int]) -> None:
+        for l in lengths:
+            subset = self.index + l
+            self.reverseSubset(self.index, subset)
+            self.index = (subset + self.skip) % self.length
+            self.skip += 1
         self.firstTwo = self.hash[0] * self.hash[1]
 
     def getFirstTwo(self) -> int:
+        self.reset()
+        self.runLengths(self.lengths)
         return self.firstTwo
+
+    def densitfy(self, hashList: List[int]) -> int:
+        return reduce(lambda x, y: x ^ y, hashList)
+
+    def generateKnotHash(self) -> str:
+        self.reset()
+        str = ""
+        for _ in range(64):
+            self.runLengths(self.newLengths)
+        for i in range(self.length // 16):
+            val = self.densitfy(self.hash[i * 16 : (i + 1) * 16])
+            str += format(val, "02x")
+        return str
 
 
 if __name__ == "__main__":
     knot = Knot()
     print("Day 10 part 1:", knot.getFirstTwo())
-    # print("Day 10 part 2:", knot.getGarbage())
+    print("Day 10 part 2:", knot.generateKnotHash())
