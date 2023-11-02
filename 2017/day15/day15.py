1bcdaed094b1ff42fd071bacbde2347ed6923103
fchoi1 <fchoi@outlook.com>
2023-11-01 20:48:45 -0400

    done day 15 of 2017

--- 2017/day15/day15.py
+++ 2017/day15/day15.py
@@ -2,20 +2,50 @@ from typing import Optional, List
 
 
 class Generators:
-    def getInput(self) -> str:
+    def getInput(self) -> List[int]:
         inputFile = "input-test.txt" if self.useTest else "input.txt"
         with open(inputFile, "r") as file1:
-            return file1.readline().strip()
+            return [int(x.strip().split(" ")[-1]) for x in file1.readlines()]
 
     def __init__(self, useTest: Optional[bool] = False) -> None:
         self.useTest = useTest
-        self.key = self.getInput()
+        self.genA, self.genB = self.getInput()
+        self.factorA = 16807
+        self.factorB = 48271
+        self.product = 2147483647
 
-    def getUsed(self) -> int:
-        return 1
+    def compare16Bit(self, num1: int, num2: int) -> bool:
+        n1 = num1 & 0xFFFF
+        n2 = num2 & 0xFFFF
+        return n1 == n2
+
+    def getNextValue(self, num: int, gen: str, isPart2: bool) -> int:
+        multiple = 4 if gen == "A" else 8
+        factor = self.factorA if gen == "A" else self.factorB
+        num = (num * factor) % self.product
+
+        if not isPart2:
+            return num
+        
+        while num % multiple != 0:
+            num = (num * factor) % self.product
+        return num
+
+    def getPairs(self, isPart2: Optional[bool] = False) -> int:
+        self.genA, self.genB = self.getInput()
+        loops = 5_000_000 if isPart2 else 40_000_000
+        count = pairs = 0
+        while count < loops:
+            if self.compare16Bit(self.genA, self.genB):
+                pairs += 1
+            self.genA = self.getNextValue(self.genA, "A", isPart2)
+            self.genB = self.getNextValue(self.genB, "B", isPart2)
+            count += 1
+        return pairs
 
 
 if __name__ == "__main__":
-    generators = Generators(True)
-    print("Day 15 part 1:", Generators.getUsed())
-    # print("Day 15 part 2:", generators.getRegions())
+    generators = Generators()
+    print("Day 15 part 1:", generators.getPairs())
+    print("Day 15 part 2:", generators.getPairs(True))
+    # Total Runtime ~ 40s
