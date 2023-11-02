fe8d6488b0801a6ff5f5cce6fc857b9344d07cb1
fchoi1 <fchoi@outlook.com>
2023-11-02 00:49:49 -0400

    done day 17 of 2017

---
+++ 2017/day17/day17.py
@@ -0,0 +1,55 @@
+from typing import Optional
+
+
+class Node:
+    def __init__(self, val: int) -> None:
+        self.val = val
+        self.next = None
+
+
+class Spinlock:
+    def getInput(self) -> int:
+        inputFile = "input-test.txt" if self.useTest else "input.txt"
+        with open(inputFile, "r") as file1:
+            return int(file1.readline().strip())
+
+    def __init__(self, useTest: Optional[bool] = False) -> None:
+        self.useTest = useTest
+        self.step = self.getInput()
+        self.pos = 0
+
+    def printNode(self, node):
+        val = node.val
+        node = node.next
+        arr = [val]
+        while node.val != val:
+            arr.append(node.val)
+            node = node.next
+        print(arr)
+
+    def getPosition(self) -> int:
+        node = Node(0)
+        node.next = node
+        for i in range(1, 2018):
+            for _ in range(self.step):
+                node = node.next
+            newNode = Node(i)
+            node.next, newNode.next = newNode, node.next
+            node = newNode
+        return node.next.val
+
+    def getPosition2(self) -> int:
+        pos1Val = 0
+        currPos = 0
+        for i in range(1, 50_000_000):
+            currPos = ((currPos + self.step) % i + 1) % (i + 1)
+            if currPos == 1:
+                pos1Val = i
+        return pos1Val
+
+
+if __name__ == "__main__":
+    spinlock = Spinlock()
+    print("Day 17 part 1:", spinlock.getPosition())
+    print("Day 17 part 2:", spinlock.getPosition2())
+    # Total Runtime ~6.15s
