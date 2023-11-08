109a3ca1334eb1901b4d57f9cef11cca8865002f
fchoi1 <fchoi@outlook.com>
2023-11-08 02:03:45 -0500

    Done 2017 AOC

--- 2017/day25/day25.py
+++ 2017/day25/day25.py
@@ -1,67 +1,65 @@
-from typing import Optional, List, Dict, Set
-from collections import defaultdict
+from typing import Optional, Tuple
 
 
-class Moat:
-    def getInput(self) -> List[List[str]]:
+class StateAction:
+    def __init__(self, val: bool, direction: int, nextState: str) -> None:
+        self.val = val
+        self.dir = direction
+        self.next = nextState
+
+
+class State:
+    def __init__(self, zeroAction: StateAction, oneAction: StateAction) -> None:
+        self.zero = zeroAction
+        self.one = oneAction
+
+
+class Turing:
+    def getInput(self) -> Tuple:
         inputFile = "input-test.txt" if self.useTest else "input.txt"
-        bridges = defaultdict(set)
+        begin = ""
+        steps = 0
         with open(inputFile, "r") as file1:
             for line in file1.readlines():
-                line = line.strip().split("/")
-                bridges[int(line[0])].add(int(line[1]))
-                bridges[int(line[1])].add(int(line[0]))
-        return bridges
+                line = line.strip()[:-1]
+                if "Begin" in line:
+                    begin = line.split(" ")[3]
+                elif "Perform" in line:
+                    steps = int(line.split(" ")[5])
+        return begin, steps
 
     def __init__(self, useTest: Optional[bool] = False) -> None:
         self.useTest = useTest
-        self.bridges = self.getInput()
-        self.strongest = self.strongestLongest = self.Longest = 0
-        self.seen = set()
-        self.dfs(0, 0, [], self.bridges)
+        self.begin, self.steps = self.getInput()
+        # Hardcode
+        if self.useTest:
+            self.states = {
+                "A": State(StateAction(True, 1, "B"), StateAction(False, -1, "B")),
+                "B": State(StateAction(True, -1, "A"), StateAction(True, 1, "A")),
+            }
+        else:
+            self.states = {
+                "A": State(StateAction(True, 1, "B"), StateAction(False, -1, "B")),
+                "B": State(StateAction(True, -1, "C"), StateAction(False, 1, "E")),
+                "C": State(StateAction(True, 1, "E"), StateAction(False, -1, "D")),
+                "D": State(StateAction(True, -1, "A"), StateAction(True, -1, "A")),
+                "E": State(StateAction(False, 1, "A"), StateAction(False, 1, "F")),
+                "F": State(StateAction(True, 1, "E"), StateAction(True, 1, "A")),
+            }
 
-    def generateKey(self, bList: List[List[int]]) -> str:
-        bList.sort(key=lambda x: (x[0], x[1]))
-        key = ""
-        for pair in bList:
-            key += f"{str(pair[0])}/{str(pair[1])}-"
-        return key
-
-    def dfs(
-        self,
-        end: int,
-        strength: int,
-        bList: List[List[int]],
-        bridges: Dict[int, Set[int]],
-    ) -> None:
-        key = self.generateKey(bList)
-        if key in self.seen:
-            return
-        self.seen.add(key)
-        if len(bridges[end]) == 0:
-            self.strongest = max(self.strongest, strength)
-            if len(bList) >= self.Longest:
-                self.Longest = len(bList)
-                self.strongestLongest = max(self.strongestLongest, strength)
-            return
-
-        for link in bridges[end].copy():
-            bList.append((min(end, link), max(end, link)))
-            bridges[link].discard(end)
-            bridges[end].discard(link)
-            self.dfs(link, strength + end + link, bList, bridges)
-            bridges[link].add(end)
-            bridges[end].add(link)
-            bList.pop()
-
-    def getStrongest(self) -> int:
-        return self.strongest
-
-    def getStrongestLongest(self) -> int:
-        return self.strongestLongest
+    def getOnes(self) -> int:
+        ones = set()
+        state = self.begin
+        pos = 0
+        for _ in range(self.steps):
+            action = self.states[state].one if pos in ones else self.states[state].zero
+            ones.add(pos) if action.val else ones.discard(pos)
+            pos += action.dir
+            state = action.next
+        return len(ones)
 
 
 if __name__ == "__main__":
-    moat = Moat()
-    print("Day 24 part 1:", moat.getStrongest())
-    print("Day 24 part 2:", moat.getStrongestLongest())
+    turing = Turing()
+    print("Day 25 part 1:", turing.getOnes())
+    # Total runtime ~2s
