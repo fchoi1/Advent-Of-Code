45af66d7ce4e8fcb4e095c5337f5d8f015d8a109
fchoi1 <fchoi@outlook.com>
2023-11-08 01:23:10 -0500

    done day 24 of 2017

--- 2017/day24/day24.py
+++ 2017/day24/day24.py
@@ -1,28 +1,67 @@
-from typing import Optional, List
+from typing import Optional, List, Dict, Set
+from collections import defaultdict
 
 
-class Coprocessor:
+class Moat:
     def getInput(self) -> List[List[str]]:
         inputFile = "input-test.txt" if self.useTest else "input.txt"
-        commands = []
+        bridges = defaultdict(set)
         with open(inputFile, "r") as file1:
             for line in file1.readlines():
-                line = line.strip().split(" ")
-                line[1:] = [
-                    int(item) if item.lstrip("-").isdigit() else item
-                    for item in line[1:]
-                ]
-                commands.append(line)
-        return commands
+                line = line.strip().split("/")
+                bridges[int(line[0])].add(int(line[1]))
+                bridges[int(line[1])].add(int(line[0]))
+        return bridges
 
     def __init__(self, useTest: Optional[bool] = False) -> None:
         self.useTest = useTest
-        self.commands = self.getInput()
+        self.bridges = self.getInput()
+        self.strongest = self.strongestLongest = self.Longest = 0
+        self.seen = set()
+        self.dfs(0, 0, [], self.bridges)
 
-    def getMulCount(self) -> int:
-        return 1
+    def generateKey(self, bList: List[List[int]]) -> str:
+        bList.sort(key=lambda x: (x[0], x[1]))
+        key = ""
+        for pair in bList:
+            key += f"{str(pair[0])}/{str(pair[1])}-"
+        return key
+
+    def dfs(
+        self,
+        end: int,
+        strength: int,
+        bList: List[List[int]],
+        bridges: Dict[int, Set[int]],
+    ) -> None:
+        key = self.generateKey(bList)
+        if key in self.seen:
+            return
+        self.seen.add(key)
+        if len(bridges[end]) == 0:
+            self.strongest = max(self.strongest, strength)
+            if len(bList) >= self.Longest:
+                self.Longest = len(bList)
+                self.strongestLongest = max(self.strongestLongest, strength)
+            return
+
+        for link in bridges[end].copy():
+            bList.append((min(end, link), max(end, link)))
+            bridges[link].discard(end)
+            bridges[end].discard(link)
+            self.dfs(link, strength + end + link, bList, bridges)
+            bridges[link].add(end)
+            bridges[end].add(link)
+            bList.pop()
+
+    def getStrongest(self) -> int:
+        return self.strongest
+
+    def getStrongestLongest(self) -> int:
+        return self.strongestLongest
 
 
 if __name__ == "__main__":
-    coprocessor = Coprocessor()
-    print("Day 24 part 1:", coprocessor.getMulCount())
+    moat = Moat()
+    print("Day 24 part 1:", moat.getStrongest())
+    print("Day 24 part 2:", moat.getStrongestLongest())
