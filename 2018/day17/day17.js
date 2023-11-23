39c750bf21937c52780566f44a313d2e5d503b1f
fchoi1 <fchoi@outlook.com>
2023-11-23 15:53:38 -0500

    done day 17 of 2018

--- 2018/day17/day17.js
+++ 2018/day17/day17.js
@@ -21,58 +21,110 @@ class Reservoir {
     this.clays = this.getInput();
     this.walls = this.getWalls();
     this.stillWater = new Set();
-    console.log(this.maxY);
+    this.running = new Set();
+    this.dropWater(500, 0, []);
+  }
+
+  printGrid() {
+    console.log("\nGRID");
+    for (let j = 0; j <= this.maxY + 1; j++) {
+      let str = "";
+      for (let i = this.minX; i < this.maxX; i++) {
+        const key = `${i},${j}`;
+        if (this.walls.has(key)) str += "#";
+        else if (this.stillWater.has(key)) str += "~";
+        else if (this.running.has(key)) str += "|";
+        else str += ".";
+      }
+      console.log(str);
+    }
   }
 
   getWalls() {
     const walls = new Set();
     let maxY = 0;
+    let maxX = 0;
+    let minX = Infinity;
+    let minY = Infinity;
     this.clays.forEach(({ x, y }) => {
       const [start, end] = y.length > 1 ? y : x;
-      const range = Array.from({ length: end - start }, (_, index) =>
-        y.length > 1 ? `${x[0]},${start + index}` : `${start + index},${y[0]}`
-      );
+      for (let i = start; i <= end; i++) {
+        const key = y.length > 1 ? `${x[0]},${i}` : `${i},${y[0]}`;
+        walls.add(key);
+      }
       maxY = Math.max(maxY, y[0], y[1] !== undefined ? y[1] : 0);
-      walls.add(...range);
+      minY = Math.min(minY, y[0], y[1] !== undefined ? y[1] : Infinity);
+      maxX = Math.max(maxX, x[0], x[1] !== undefined ? x[1] : 0);
+      minX = Math.min(minX, x[0], x[1] !== undefined ? x[1] : Infinity);
     });
     this.maxY = maxY;
+    this.minY = minY;
+    this.maxX = maxX + 1;
+    this.minX = minX - 1;
     return walls;
   }
 
-  waterFalling(x, y, runningWater) {
-    let key = `${x}, ${y}`;
-    if (!this.stillWater.has(key) || this.walls.has(key)) return [x, y];
+  dropWater(x, y, stack) {
+    let key = `${x},${y}`;
+    stack.push([x, y]);
+    while (stack.length > 0) {
+      while (y <= this.maxY && !this.stillWater.has(key) && !this.walls.has(key)) {
+        if (y >= this.minY) {
+          this.running.add(key);
+          stack.push([x, y]);
+        }
+        y += 1;
+        key = `${x},${y}`;
+      }
+      if (y > this.maxY) {
+        [x, y] = stack.pop();
+        key = `${x},${y}`;
+        while (this.running.has(key)) {
+          [x, y] = stack.pop();
+          key = `${x},${y + 1}`;
+          if (stack.length === 0) return;
+        }
+        continue;
+      }
+
+      const [floorX, floorY] = stack.pop();
+      let floorSet = new Set([`${floorX},${floorY}`]);
+      const [isRightStill, rCoords] = this.floorWater(floorX, floorY, 1, floorSet);
+      const [isLeftStill, lCoords] = this.floorWater(floorX, floorY, -1, floorSet);
+      if (isRightStill && isLeftStill) {
+        floorSet.forEach((element) => {
+          this.running.delete(element);
+          this.stillWater.add(element);
+        });
+      } else floorSet.forEach((element) => this.running.add(element));
+      if (rCoords) stack.push(rCoords);
+      if (lCoords) stack.push(lCoords);
+      if (stack.length === 0) return;
+      [x, y] = stack.pop();
+      key = `${x},${y}`;
+    }
   }
 
-  floorWater(x, y, direction, floorSet) {}
-
-  dropWater(x, y, runningWater) {
-    if (y > this.maxY) return [runningWater, [x, y]];
-    let key = `${x}, ${y + 1}`;
-
-    if (!this.stillWater.has(key) || this.walls.has(key)) {
-      const [x, y] = this.waterFalling(x, y + 1, runningWater);
+  floorWater(x, y, direction, floorSet) {
+    let key = `${x + direction},${y}`;
+    while (!this.stillWater.has(key) && !this.walls.has(key)) {
+      floorSet.add(key);
+      if (!this.stillWater.has(`${x},${y + 1}`) && !this.walls.has(`${x},${y + 1}`)) return [false, [x, y]];
+      x += direction;
+      key = `${x},${y}`;
     }
-
-    return [runningWater, [x, y]];
-
-    return [runningWater, [x, y]];
-
-    return [1, 1];
+    return [true, null];
   }
 
   getWaterCount() {
-    let flowWater = new Set();
-    let prevSize;
-    let pos = [500, 0];
-    let loops = 0;
-    while (loops < 100 && prevSize !== this.stillWater.size + flowWater.size) {
-      const [running, coord] = this.dropWater(500, 0, new Set());
-      loops++;
-    }
-    return 1;
+    return this.stillWater.size + this.running.size;
+  }
+
+  getStillWater() {
+    return this.stillWater.size;
   }
 }
 
-const reservoir = new Reservoir(true);
+const reservoir = new Reservoir();
 console.log("Day 17 part 1:", reservoir.getWaterCount());
+console.log("Day 17 part 2:", reservoir.getStillWater());
