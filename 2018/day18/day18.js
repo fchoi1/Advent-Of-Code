5a881bda929f29ac11eceb05fe0d9b701abe409d
fchoi1 <fchoi@outlook.com>
2023-11-23 21:07:07 -0500

    done day 18 of 2018

--- 2018/day18/day18.js
+++ 2018/day18/day18.js
@@ -1,16 +1,11 @@
 const fs = require("fs");
 
-class Reservoir {
+class LumberYard {
   getInput() {
     const inputFile = this.useTest ? "input-test.txt" : "input.txt";
     try {
       const data = fs.readFileSync(inputFile, "utf8").trim().split(/\r?\n/);
-      return data.map((row) => {
-        const [first, n1, second, n2, n3] = row.split(/=|,|\.\./);
-        const xCoord = first === "x" ? [parseInt(n1)] : [parseInt(n2), parseInt(n3)];
-        const yCoord = first === "x" ? [parseInt(n2), parseInt(n3)] : [parseInt(n1)];
-        return { x: xCoord, y: yCoord };
-      });
+      return data.map((row) => row.split(""));
     } catch (err) {
       throw err;
     }
@@ -18,17 +13,84 @@ class Reservoir {
 
   constructor(useTest = false) {
     this.useTest = useTest;
-    this.clays = this.getInput();
-  }
-  getWaterCount() {
-    return this.stillWater.size + this.running.size;
+    this.forestMap = this.getInput();
+    this.width = this.forestMap[0].length;
+    this.height = this.forestMap.length;
   }
 
-  getStillWater() {
-    return this.stillWater.size;
+  getAcreScore() {
+    let lumber = 0;
+    let trees = 0;
+    this.forestMap.forEach((row) => {
+      row.forEach((acre) => {
+        if (acre === "#") lumber++;
+        else if (acre === "|") trees++;
+      });
+    });
+    return lumber * trees;
+  }
+
+  checkAcre(x, y) {
+    const dirMap = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+    ];
+    let trees = 0;
+    let lumber = 0;
+    for (const [dx, dy] of dirMap) {
+      const newX = x + dx;
+      const newY = y + dy;
+      if (newX >= 0 && newX < this.width && newY >= 0 && newY < this.height) {
+        if (this.forestMap[newY][newX] === "#") lumber++;
+        else if (this.forestMap[newY][newX] === "|") trees++;
+      }
+    }
+    const acre = this.forestMap[y][x];
+    if (acre === ".") return trees >= 3 ? "|" : acre;
+    else if (acre === "|") return lumber >= 3 ? "#" : acre;
+    else if (acre === "#") return trees >= 1 && lumber >= 1 ? "#" : ".";
+  }
+
+  printMap() {
+    this.forestMap.forEach((row) => console.log(row.join("")));
+  }
+
+  getResources(isPart2) {
+    this.forestMap = this.getInput();
+    let time = 0;
+    let maxTime = isPart2 ? 1_000_000_000 : 10;
+    const repeated = 5;
+    const scoreList = [];
+    let score, key;
+    const seen = {};
+    while (time < maxTime) {
+      const temp = [];
+      for (let j = 0; j < this.height; j++) {
+        const row = [];
+        for (let i = 0; i < this.width; i++) row.push(this.checkAcre(i, j));
+        temp.push(row);
+      }
+      this.forestMap = temp;
+      score = this.getAcreScore();
+      scoreList.push(score);
+      key = scoreList.slice(-repeated).join(",");
+      time++;
+      if (seen[key]) break;
+      seen[key] = time;
+    }
+    if (maxTime === time) return score;
+    const loopLength = time - seen[key];
+    const looped = scoreList.slice(-loopLength);
+    return looped[((maxTime - time) % loopLength) - 1];
   }
 }
 
-const reservoir = new Reservoir();
-console.log("Day 17 part 1:", reservoir.getWaterCount());
-console.log("Day 17 part 2:", reservoir.getStillWater());
+const lumberYard = new LumberYard();
+console.log("Day 18 part 1:", lumberYard.getResources());
+console.log("Day 18 part 2:", lumberYard.getResources(true));
