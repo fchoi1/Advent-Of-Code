33e054d8ead466385806754134a774ab170cc01f
fchoi1 <fchoi@outlook.com>
2023-12-15 01:18:01 -0500

    done day 15 of  2023

---
+++ 2023/day15/day15.py
@@ -0,0 +1,57 @@
+from typing import List, Optional
+
+
+class Library:
+    def getInput(self) -> List[List[List[str]]]:
+        inputFile = "input-test.txt" if self.useTest else "input.txt"
+        with open(inputFile, "r") as file1:
+            return file1.readline().strip().split(",")
+
+    def __init__(self, useTest: Optional[bool] = False) -> None:
+        self.useTest = useTest
+        self.data = self.getInput()
+        self.curr = 0
+        self.boxes = [{"list": []} for _ in range(256)]
+
+    def getHash(self, string) -> int:
+        hashValue = 0
+        for char in string:
+            hashValue = ((hashValue + ord(char)) * 17) % 256
+        return hashValue
+
+    def getHashSum(self) -> int:
+        return sum(self.getHash(line) for line in self.data)
+
+    def getTotal(self) -> int:
+        total = 0
+        for line in self.data:
+            label, val = line.split("=") if "=" in line else line.split("-")
+            box = self.getHash(label)
+            currBox = self.boxes[box]
+
+            if val:
+                if label in currBox:
+                    index = currBox[label]
+                    currBox["list"][index] = int(val)
+                else:
+                    currBox[label] = len(currBox["list"])
+                    currBox["list"].append(int(val))
+            elif label in currBox:
+                index = currBox[label]
+                currBox["list"][index] = 0
+                del currBox[label]
+
+        for i, box in enumerate(self.boxes):
+            rank = 0
+            for val in box["list"]:
+                if val == 0:
+                    continue
+                rank += 1
+                total += val * rank * (i + 1)
+        return total
+
+
+if __name__ == "__main__":
+    library = Library()
+    print("Day 15 part 1:", library.getHashSum())
+    print("Day 15 part 1:", library.getTotal())
