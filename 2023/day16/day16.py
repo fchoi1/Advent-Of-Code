e22756c15133f94b4d4cfc63003e79a1eeb76d1f
fchoi1 <fchoi@outlook.com>
2023-12-16 01:07:36 -0500

    done day 16 of 2023

--- 2023/day16/day16.py
+++ 2023/day16/day16.py
@@ -1,35 +1,90 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple, Set
 
 
-class Library:
+class Lava:
     def getInput(self) -> List[List[List[str]]]:
         inputFile = "input-test.txt" if self.useTest else "input.txt"
-        laser = []
         with open(inputFile, "r") as file1:
-            for line in file1:
-                line = line.strip()
-                laser.append(list(line))
-
-            return laser
+            return [list(x.strip()) for x in file1]
 
     def __init__(self, useTest: Optional[bool] = False) -> None:
         self.useTest = useTest
-        self.data = self.getInput()
-        self.energized = set()
-        print(self.data)
+        self.map = self.getInput()
+        self.w = len(self.map[0])
+        self.h = len(self.map)
 
-    def moveBeam(self, direction, pos):
+    def inBounds(self, x, y):
+        return 0 <= x < self.w and 0 <= y < self.h
+
+    def moveBeam(
+        self,
+        d: Tuple[int],
+        pos: Tuple[int],
+        seen: Set[Tuple[int]],
+        energy: Set[Tuple[int]],
+    ):
+        dx, dy = d
         x, y = pos
-        key = 
-        pass
+        if (x, y, d) in seen:
+            return seen, energy
+        seen.add((x, y, d))
+        while self.inBounds(x, y) and self.map[y][x] == ".":
+            energy.add((x, y))
+            x, y = x + dx, y + dy
 
-    def getTotal(self) -> int:
-        total = 0
+        if not self.inBounds(x, y):
+            return seen, energy
+        energy.add((x, y))
+        sym = self.map[y][x]
 
-        return len(self.energized)
+        for d, p in self.getMoveList(sym, d, (x, y)):
+            seen, energy = self.moveBeam(d, p, seen, energy)
+        return seen, energy
+
+    def getMoveList(self, sym: str, d: Tuple[int], pos: Tuple[int]):
+        m = []
+        dx, dy = d
+        x, y = pos
+        if dy == 0:
+            if sym == "-":
+                m.append((d, (x + dx, y)))
+            elif sym == "/" and dx == 1 or sym == "\\" and dx == -1:
+                m.append(((0, -1), (x, y - 1)))
+            elif sym == "/" and dx == -1 or sym == "\\" and dx == 1:
+                m.append(((0, 1), (x, y + 1)))
+            elif sym == "|":
+                m.append(((0, 1), (x, y + 1)))
+                m.append(((0, -1), (x, y - 1)))
+        else:
+            if sym == "|":
+                m.append((d, (x, y + dy)))
+            elif sym == "/" and dy == 1 or sym == "\\" and dy == -1:
+                m.append(((-1, 0), (x - 1, y)))
+            elif sym == "/" and dy == -1 or sym == "\\" and dy == 1:
+                m.append(((1, 0), (x + 1, y)))
+            elif sym == "-":
+                m.append(((-1, 0), (x - 1, y)))
+                m.append(((1, 0), (x + 1, y)))
+        return m
+
+    def getE(
+        self,
+        direction: Optional[Tuple[int, int]] = (1, 0),
+        start: Optional[Tuple[int, int]] = (0, 0),
+    ) -> int:
+        return len(self.moveBeam(direction, start, set(), set())[1])
+
+    def getMax(self):
+        t = 0
+        for i in range(len(self.map)):
+            t = max(t, self.getE((0, 1), (i, 0)), self.getE((0, -1), (i, self.h - 1)))
+        for i in range(len(self.map[0])):
+            t = max(t, self.getE((1, 0), (0, i)), self.getE((-1, 0), (self.w - 1, i)))
+        return t
 
 
 if __name__ == "__main__":
-    library = Library(True)
-    print("Day 16 part 1:", library.getTotal())
-    print("Day 16 part 1:", library.getTotal())
+    lava = Lava()
+    print("Day 16 part 1:", lava.getE())
+    print("Day 16 part 2:", lava.getMax())
+    # Total runtime ~1.67s
