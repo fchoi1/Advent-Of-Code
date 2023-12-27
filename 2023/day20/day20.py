7f2b9abe43695cc4976085889f62e4e7bcb5e48a
fchoi1 <fchoi@outlook.com>
2023-12-27 10:40:27 -0500

    starting dat 20

--- 2023/day20/day20.py
+++ 2023/day20/day20.py
@@ -1,4 +1,5 @@
 from typing import List, Optional, Tuple, Dict
+from collections import deque, defaultdict
 
 
 class Aplenty:
@@ -25,17 +26,51 @@ class Aplenty:
     def __init__(self, useTest: Optional[bool] = False) -> None:
         self.useTest = useTest
         self.broadast, self.flip, self.conj = self.getInput()
-        print(self.broadast, self.flip, self.conj)
+        self.setup()
+        print(self.broadast, self.flip, self.conj, self.val, self.Cin)
 
-    def getTotal(self) -> int:
-        total = 0
-        self.F = {}
-        self.C = {}
-        for key in self.flip.keys():
-            self.F[key] = 0
-        for key in self.conj.keys():
-            self.C[key] = 0
+    def setup(self) -> int:
+        self.val = {}
+        self.Cin = defaultdict(list)
 
+        def process(mapping):
+            for key, outputs in mapping.items():
+                self.val[key] = 0
+                for n in outputs:
+                    if n in self.conj:
+                        self.Cin[n].append(key)
+
+        process(self.flip)
+        process(self.conj)
+
+    def broadcast(self):
+        signals = [(x, 0) for x in self.broadast]
+        q = deque(signals)
+        c = 0
+        self.counts[0] += len(self.broadast)
+        while q and c < 8:
+            c += 1
+            curr, signal = q.popleft()
+            self.counts[signal] +=1  
+            if curr in self.flip:
+                if signal:
+                    continue
+                self.val[curr] = 1 if not self.val[curr] else 0
+                s = [(x, self.val[curr]) for x in self.flip[curr]]
+            elif curr in self.conj:
+                self.val[curr] = 0 if all(self.val[x] for x in self.Cin[curr]) else 1
+                s = [(x, self.val[curr]) for x in self.conj[curr]]
+            q.extend(s)
+            print(curr, self.val, q)
+        print(q, self.val, s)
+
+    def getTotal(self):
+        total = 1
+        self.counts = [0, 0]
+        for _ in range(1):
+            print()
+            self.broadcast()
+        print(self.counts)
         return total
 
 
