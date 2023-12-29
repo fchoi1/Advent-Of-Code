5610df67fc76cd235bbe7f32dad1e27bd39786ea
fchoi1 <fchoi@outlook.com>
2023-12-29 14:31:06 -0500

    udpated day 21

--- 2023/day21/day21.py
+++ 2023/day21/day21.py
@@ -21,9 +21,9 @@ class StepCounter:
     def inBounds(self, pos: Tuple[int]) -> bool:
         return 0 <= pos[0] < len(self.map[0]) and 0 <= pos[1] < len(self.map)
 
-    def bfs(self, start: List[Tuple[int]], target: Optional[int] = None) -> int:
+    def bfs(self, start: Tuple[int], target: Optional[int] = None) -> int:
         count, step = 0, 0
-        q = start
+        q = [start]
         seen = set()
         while q:
             temp = []
@@ -45,30 +45,30 @@ class StepCounter:
         return count
 
     def getSteps(self) -> int:
-        return self.bfs([(self.sx, self.sy)], 7 if self.useTest else 65)
+        return self.bfs((self.sx, self.sy), 7 if self.useTest else 65)
 
     def getSteps2(self) -> int:
         size = len(self.map)
-        oddCount = self.bfs([(self.sx, self.sy)])
-        evenCount = self.bfs([(self.sx + 1, self.sy)])
+        oddCount = self.bfs((self.sx, self.sy))
+        evenCount = self.bfs((self.sx + 1, self.sy))
 
         ss = size // 2
-        small_tl = self.bfs([(size - 1, size - 1)], ss)
-        small_tr = self.bfs([(0, size - 1)], ss)
-        small_bl = self.bfs([(size - 1, 0)], ss)
-        small_br = self.bfs([(0, 0)], ss)
+        small_tl = self.bfs((size - 1, size - 1), ss)
+        small_tr = self.bfs((0, size - 1), ss)
+        small_bl = self.bfs((size - 1, 0), ss)
+        small_br = self.bfs((0, 0), ss)
 
         # even case
-        bs = 3 * size // 2 - 1
-        big_tl = self.bfs([(size - 1, size - 2), (size - 2, size - 1)], bs)
-        big_tr = self.bfs([(0, size - 2), (1, size - 1)], bs)
-        big_bl = self.bfs([(size - 2, 0), (size - 1, 1)], bs)
-        big_br = self.bfs([(0, 1), (1, 0)], bs)
+        bs = 3 * size // 2
+        big_tl = self.bfs((size - 1, size), bs)
+        big_tr = self.bfs((-1, size - 1), bs)
+        big_bl = self.bfs((size - 1, -1), bs)
+        big_br = self.bfs((-1, 0), bs)
 
-        top = self.bfs([(self.sx, size - 1)], size)
-        bot = self.bfs([(self.sx, 0)], size)
-        left = self.bfs([(size - 1, self.sy)], size)
-        right = self.bfs([(0, self.sy)], size)
+        top = self.bfs((self.sx, size - 1), size)
+        bot = self.bfs((self.sx, 0), size)
+        left = self.bfs((size - 1, self.sy), size)
+        right = self.bfs((0, self.sy), size)
 
         steps = 5000 if self.useTest else 26501365
         gridWidth = steps // size
@@ -77,7 +77,8 @@ class StepCounter:
         return (
             odd * oddCount
             + even * evenCount
-            + (gridWidth - 1) * (big_tl + big_tr + big_bl + big_br)
+            + (gridWidth - 1)
+            * (big_tl + big_tr + big_bl + big_br - 4)  # start from outside box
             + gridWidth * (small_tl + small_tr + small_bl + small_br)
             + (top + bot + left + right)
         )
