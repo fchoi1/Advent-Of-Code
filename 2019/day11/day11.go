1def68c62dd53ddf1fd0f815319e036fd4e55b1a
fchoi1 <fchoi@outlook.com>
2024-01-31 01:24:08 -0500

    done day 11 of 2019

--- 2019/day11/day11.go
+++ 2019/day11/day11.go
@@ -3,169 +3,211 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
 
-type Monitor struct {
+type PaintMap struct {
 	UseTest      bool
-	rockMap      [][]rune
-	asteroids    [][]int
-	currAsteroid []int
-	maxCount     int
-	asteroid200  int
+	IntCode      []int
+	relativeBase int
+	maxRange     []int
+	minRange     []int
+	count        int
+	white        map[string]bool
 }
 
-type AsteroidResult struct {
-	count int
-	seen  map[string][]int
+func mod(a, b int) int {
+	return (a%b + b) % b
 }
 
-func getDist(loc1, loc2 []int) int {
-	return abs(loc1[0]-loc2[0]) + abs(loc1[1]-loc2[1])
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func parseNumbers(key string) []float64 {
-	parts := strings.Split(key, ",")
-	num1, _ := strconv.ParseFloat(parts[0], 64)
-	num2, _ := strconv.ParseFloat(parts[1], 64)
-	return []float64{num2, num1}
-}
-
-func parseMap(locMap map[string][]int) [][]float64 {
-	asteroidList := [][]float64{}
-	for key, value := range locMap {
-		parsed := parseNumbers(key)
-		parsed = append(parsed, float64(value[0]), float64(value[1]))
-		asteroidList = append(asteroidList, parsed)
-	}
-	sortFunc := func(i, j int) bool {
-		if asteroidList[i][0] == asteroidList[j][0] {
-			return asteroidList[i][1] < asteroidList[j][1]
-		}
-		return asteroidList[i][0] > asteroidList[j][0]
-	}
-	sort.Slice(asteroidList, sortFunc)
-	return asteroidList
-}
-
-func parseAndReplaceInfinity(str string) int {
-	if str == "+Inf" {
-		return math.MaxInt
-	} else if str == "-Inf" {
-		return math.MinInt
-	} else {
-		num, _ := strconv.Atoi(str)
-		return num
-	}
-}
-
-func (this *Monitor) getInput() {
+func (this *PaintMap) getInput() {
 	inputFile := "input.txt"
 	if this.UseTest {
 		inputFile = "input-test.txt"
 	}
 	file, _ := os.Open(inputFile)
 	scanner := bufio.NewScanner(file)
+	this.IntCode = []int{}
 	for scanner.Scan() {
 		line := scanner.Text()
-		this.rockMap = append(this.rockMap, []rune(line))
+		strNum := strings.Split(line, ",")
+		for _, n := range strNum {
+			num, _ := strconv.Atoi(n)
+			this.IntCode = append(this.IntCode, num)
+		}
 	}
-	this.asteroids = this.getAsteroids()
+	this.white = make(map[string]bool)
+	this.maxRange = []int{0, 0}
+	this.minRange = []int{0, 0}
+	for i := 0; i < 450; i++ {
+		this.IntCode = append(this.IntCode, 0)
+	}
 	defer file.Close()
 }
-
-func (this *Monitor) getAsteroids() [][]int {
-	asteroids := [][]int{}
-	for y, row := range this.rockMap {
-		for x, val := range row {
-			if val == '#' {
-				asteroids = append(asteroids, []int{x, y})
-			}
-		}
+func (this *PaintMap) parseOpCode(n int) (int, []int) {
+	code := n % 100
+	rest := strconv.Itoa(n / 100)
+	arr := []int{}
+	for i := len(rest) - 1; i >= 0; i-- {
+		intValue, _ := strconv.Atoi(string(rest[i]))
+		arr = append(arr, intValue)
 	}
-	return asteroids
+	for len(arr) < 3 {
+		arr = append(arr, 0)
+	}
+	return code, arr
 }
 
-func (this *Monitor) getVisible(currAsteroid []int) AsteroidResult {
-	count := 0
-	seen := make(map[string][]int)
-	currX, currY := currAsteroid[0], currAsteroid[1]
-	for _, asteroid := range this.asteroids {
-		x, y := asteroid[0], asteroid[1]
-		if x == currX && y == currY {
+func (this *PaintMap) runProgram(index int, intCode []int, input []int) ([]int, int) {
+	output := []int{}
+
+	for index < len(intCode) {
+		code, params := this.parseOpCode(intCode[index])
+		if code == 99 {
+			return output, -1
+		}
+
+		a, b, c := intCode[index+1], intCode[index+2], intCode[index+3]
+
+		if params[0] == 0 {
+			a = intCode[a]
+		} else if params[0] == 2 {
+			a = intCode[a+this.relativeBase]
+		}
+
+		switch code {
+		case 3, 4:
+			if code == 3 {
+				if len(input) == 0 {
+					return output, index
+				}
+				if params[0] == 2 {
+					intCode[intCode[index+1]+this.relativeBase] = input[0]
+				} else {
+					intCode[intCode[index+1]] = input[0]
+				}
+				input = input[1:]
+			} else if code == 4 {
+				output = append(output, a)
+			}
+			index += 2
+			continue
+		case 9:
+			this.relativeBase += a
+			index += 2
 			continue
 		}
-		slope := float64(y-currY) / float64(x-currX)
-		result := "0"
-		if x > currX || (x == currX && y <= currY) {
-			result = "1"
+
+		if params[1] == 0 {
+			b = intCode[b]
+		} else if params[1] == 2 {
+			b = intCode[b+this.relativeBase]
 		}
-		if math.IsInf(slope, 1) {
-			slope = math.Inf(-1)
+
+		if params[2] == 2 {
+			c += this.relativeBase
 		}
-		slopeStr := strconv.FormatFloat(slope, 'f', -1, 64)
-		key := slopeStr + "," + result
-		_, exists := seen[key]
-		if !exists {
-			count++
-			seen[key] = asteroid
+		switch code {
+		case 1:
+			intCode[c] = a + b
+		case 2:
+			intCode[c] = a * b
+		case 5, 6:
+			if (code == 5 && a != 0) || (code == 6 && a == 0) {
+				index = b
+			} else {
+				index += 3
+			}
+			continue
+		case 7, 8:
+			intCode[c] = 0
+			if (code == 7 && a < b) || (code == 8 && a == b) {
+				intCode[c] = 1
+			} else {
+				intCode[c] = 0
+			}
 		}
-		if getDist(currAsteroid, asteroid) < getDist(currAsteroid, seen[key]) {
-			seen[key] = asteroid
-		}
+		index += 4
 	}
-	return AsteroidResult{count, seen}
+	return output, -1
 }
 
-func (this *Monitor) analyzeAsteroids() {
-	maxCount := 0
-	currAsteroid := []int{}
-	destroyLimit := 200
-	currSeen := make(map[string][]int)
-	for _, asteroid := range this.asteroids {
-		AR := this.getVisible(asteroid)
-		if maxCount < AR.count {
-			maxCount = AR.count
-			currAsteroid = asteroid
-			currSeen = AR.seen
+func (this *PaintMap) paintRobot(initialInput int) {
+	input, nextIndex, direction := initialInput, 0, 0
+	location := []int{0, 0}
+	dirMap := [][]int{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
+	black := make(map[string]bool)
+	var out []int
+	strKey := "0,0"
+	for nextIndex != -1 {
+		for i := 0; i < 2; i++ {
+			if location[i] > this.maxRange[i] {
+				this.maxRange[i] = location[i]
+			}
+			if location[i] < this.minRange[i] {
+				this.minRange[i] = location[i]
+			}
+		}
+		out, nextIndex = this.runProgram(nextIndex, this.IntCode, []int{input})
+		_, blackExists := black[strKey]
+		_, whiteExists := this.white[strKey]
+		if !blackExists && !whiteExists {
+			this.count++
+		}
+		if out[0] == 0 {
+			black[strKey] = true
+			delete(this.white, strKey)
+		} else {
+			this.white[strKey] = true
+			delete(black, strKey)
+		}
+		if out[1] == 1 {
+			direction = mod(direction+1, 4)
+		} else {
+			direction = mod(direction-1, 4)
+		}
+		location[0] += dirMap[direction][0]
+		location[1] += dirMap[direction][1]
+		strKey = strconv.Itoa(location[0]) + "," + strconv.Itoa(location[1])
+		if this.white[strKey] {
+			input = 1
+		} else {
+			input = 0
 		}
 	}
-	this.currAsteroid = currAsteroid
-	this.maxCount = maxCount
-	asteroidList := parseMap(currSeen)
+}
 
-	if len(asteroidList) < destroyLimit {
-		fmt.Println("loop again") // Too lazy to implement
-	} else {
-		this.asteroid200 = int(asteroidList[destroyLimit-1][2]*100 + asteroidList[destroyLimit-1][3])
+func (this *PaintMap) countPanels() int {
+	this.getInput()
+	this.paintRobot(0)
+	return this.count
+}
+
+func (this *PaintMap) printPanels() {
+	this.getInput()
+	this.paintRobot(1)
+	for y := this.minRange[1]; y <= this.maxRange[1]; y++ {
+		for x := this.minRange[0]; x < this.maxRange[0]; x++ {
+			strKey := strconv.Itoa(x) + "," + strconv.Itoa(y)
+			if this.white[strKey] {
+				fmt.Print("# ")
+			} else {
+				fmt.Print(". ")
+			}
+		}
+		fmt.Println()
 	}
-}
-
-func (this *Monitor) getMaxCount() int {
-	return this.maxCount
-}
-
-func (this *Monitor) get200th() int {
-	return this.asteroid200
 }
 
 func main() {
-	monitor := &Monitor{
+	paintMap := &PaintMap{
 		UseTest: false,
+		IntCode: []int{},
 	}
-	monitor.getInput()
-	monitor.analyzeAsteroids()
-	fmt.Println("Day 9 part 1:", monitor.getMaxCount())
-	fmt.Println("Day 9 part 2:", monitor.get200th())
+	paintMap.getInput()
+	fmt.Println("Day 11 part 1:", paintMap.countPanels())
+	fmt.Println("Day 11 part 2:")
+	paintMap.printPanels()
 }
