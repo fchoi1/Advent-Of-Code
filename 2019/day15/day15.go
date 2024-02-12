ccbb9a7f9e34ed1e9e2cbc98bfa5f03a7ac526c7
fchoi1 <fchoi@outlook.com>
2024-02-12 16:34:06 -0500

    done day 16 of 2019 some bs

---
+++ 2019/day15/day15.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type GameMap struct {
+	UseTest      bool
+	IntCode      []int
+	relativeBase int
+	grid         map[string]rune
+}
+type Program struct {
+	IntCode []int
+}
+
+func (this *GameMap) getInput() {
+	inputFile := "input.txt"
+	if this.UseTest {
+		inputFile = "input-test.txt"
+	}
+	file, _ := os.Open(inputFile)
+	scanner := bufio.NewScanner(file)
+	this.IntCode = []int{}
+	this.relativeBase = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		strNum := strings.Split(line, ",")
+		for _, n := range strNum {
+			num, _ := strconv.Atoi(n)
+			this.IntCode = append(this.IntCode, num)
+		}
+		// additional space need
+		for i := 0; i < 15; i++ {
+			this.IntCode = append(this.IntCode, 0)
+		}
+	}
+	defer file.Close()
+}
+func (this *GameMap) parseOpCode(n int) (int, []int) {
+	code := n % 100
+	rest := strconv.Itoa(n / 100)
+	arr := []int{}
+	for i := len(rest) - 1; i >= 0; i-- {
+		intValue, _ := strconv.Atoi(string(rest[i]))
+		arr = append(arr, intValue)
+	}
+	for len(arr) < 3 {
+		arr = append(arr, 0)
+	}
+	return code, arr
+}
+
+func (this *GameMap) runProgram(index int, intCode []int, input []int) ([]int, int) {
+	output := []int{}
+
+	for index < len(intCode) {
+		code, params := this.parseOpCode(intCode[index])
+		if code == 99 {
+			return output, -1
+		}
+
+		a, b, c := intCode[index+1], intCode[index+2], intCode[index+3]
+
+		if params[0] == 0 {
+			a = intCode[a]
+		} else if params[0] == 2 {
+			a = intCode[a+this.relativeBase]
+		}
+
+		switch code {
+		case 3, 4:
+			if code == 3 {
+				if len(input) == 0 {
+					return output, index
+				}
+				if params[0] == 2 {
+					intCode[intCode[index+1]+this.relativeBase] = input[0]
+				} else {
+					intCode[intCode[index+1]] = input[0]
+				}
+				input = input[1:]
+			} else if code == 4 {
+				output = append(output, a)
+			}
+			index += 2
+			continue
+		case 9:
+			this.relativeBase += a
+			index += 2
+			continue
+		}
+
+		if params[1] == 0 {
+			b = intCode[b]
+		} else if params[1] == 2 {
+			b = intCode[b+this.relativeBase]
+		}
+
+		if params[2] == 2 {
+			c += this.relativeBase
+		}
+		switch code {
+		case 1:
+			intCode[c] = a + b
+		case 2:
+			intCode[c] = a * b
+		case 5, 6:
+			if (code == 5 && a != 0) || (code == 6 && a == 0) {
+				index = b
+			} else {
+				index += 3
+			}
+			continue
+		case 7, 8:
+			intCode[c] = 0
+			if (code == 7 && a < b) || (code == 8 && a == b) {
+				intCode[c] = 1
+			} else {
+				intCode[c] = 0
+			}
+		}
+		index += 4
+	}
+	return output, -1
+}
+
+func (this *GameMap) countPanels() int {
+	this.getInput()
+	fmt.Println(len(this.IntCode))
+	return 1
+}
+
+func (this *GameMap) bfs() int {
+	coordMap := make(map[string]Program)
+	fmt.Println(coordMap)
+	return 1
+}
+
+// func (this *GameMap) getScore() int {
+// 	this.getInput()
+// 	return 1
+// }
+
+func main() {
+	gameMap := &GameMap{
+		UseTest: false,
+		IntCode: []int{},
+	}
+	fmt.Println("Day 15 part 1:", gameMap.countPanels())
+	// fmt.Println("Day 13 part 2:", gameMap.getScore())
+}
--- 2019/day16/day16.go
+++ 2019/day16/day16.go
@@ -8,13 +8,14 @@ import (
 	"strings"
 )
 
-type GameMap struct {
+type FFT struct {
 	UseTest   bool
 	numstr    string
 	numLength int
+	sumVal    int
 }
 
-func (this *GameMap) getInput() {
+func (this *FFT) getInput() {
 	inputFile := "input.txt"
 	if this.UseTest {
 		inputFile = "input-test.txt"
@@ -25,22 +26,10 @@ func (this *GameMap) getInput() {
 		this.numstr = scanner.Text()
 	}
 	this.numLength = len(this.numstr)
-	fmt.Println(len(this.numstr))
 	defer file.Close()
 }
 
-func (this *GameMap) getFullPhaseVal(strNum string) []int {
-	phaseVal := []int{}
-	str := strings.Repeat(strNum, this.numLength)
-	for i := 0; i < this.numLength; i++ {
-		length := (4 * (i + 1))
-		val := this.getPhaseVal(i, str[:length])
-		phaseVal = append(phaseVal, val)
-	}
-	return phaseVal
-}
-
-func (this *GameMap) getPhaseVal(index int, strNum string) int {
+func (this *FFT) getPhaseVal(index int, strNum string) int {
 	add := index
 	sub := 2 + index*3
 	length := (4 * (index + 1))
@@ -64,117 +53,67 @@ func (this *GameMap) getPhaseVal(index int, strNum string) int {
 	}
 	return val
 }
-func (this *GameMap) FFT(strNum string) string {
+func (this *FFT) FFT(strNum string) string {
 	var numStr string
 	for i := 0; i < this.numLength; i++ {
-		length := (4 * (i + 1))
 		val := this.getPhaseVal(i, strNum)
 		if val < 0 {
 			val *= -1
 		}
-		fmt.Println("\nnum", i, "val", val, this.numLength, length)
-		fmt.Println("calcs", this.numLength/length, this.numLength%length)
-		fmt.Println("calc2", length/this.numLength, length%this.numLength)
-
 		lastDigit := val % 10
 		numStr += strconv.Itoa(lastDigit)
 	}
-	return numStr
+	return numStr[:8]
 }
 
-func (this *GameMap) runFFT() string {
-	phases := 1
+func (this *FFT) run() string {
 	str := this.numstr
-	var i int
-	for i < phases {
+	for i := 0; i < 100; i++ {
 		str = this.FFT(str)
-		i += 1
 	}
-	// fmt.Println(str[:8], str[8:16])
 	return str
 }
 
-// 1 2 3 4   5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
-// + . - .   + . - . + . - . + . - . + . - . + . - . + . - . + . - . + . - . + . - .
-// 4 * 5
-// 1 2 3 4 5 6 7 8
-// . + + . . - - .
-// 8 * 5
-// 1 2 3 4 5 6 7 8 1 2 3 4
-// . . + + + . . . - - - .
-// 12 - 6 = 6 * 3 + 7 = 18 + 7 = 25
-// 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
-// . . . + + + + . . . . - - - - .
-
-// 4 + 5 + 6 + 7
-
-// 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
-// . . . . . . . + + + + + + + . . . . . . . - - - - - - - . . . . . . . + + + + +
-// 1 2 3 4 5 6 7 8 (1 2 3 4 5 6 7 8) 1 2 3 4 5 6 7 8 1 2 3 (4 5 6 7 8 1 2 3) 4 5 6 7 8
-// . . . . . . . .  + + + + + + + +  + . . . . . . . . . -  - - - - - - - - . . . . .
-// 1 2 3 4 5 6 7 8 1 (2 3 4 5 6 7 8 1) 2 3 4 5 6 7 8 1 2 3 4 5 6 7 (8 1 2 3 4 5 6 7 8)
-// . . . . . . . . .  + + + + + + + +  + + . . . . . . . . . . - -  - - - - - - - - .
-// 1 2 3 4 5 6 7 8 1 2 (3 4 5 6 7 8 1 2) 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
-// . . . . . . . . . .  + + + + + + + +  + + + . . . . . . . . . . . - - - - - - - -
-// 1 2 3 4 5 6 7 8 1 2 3 (4 5 6 7 8) 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 ||1 2 3 4 5 6 7
-// . . . . . . . . . . .  + + + + +  + + + + + + + . . . . . . . . . . . . - - - - - ||- - - - - - -
-// 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 ||1 2 3 4 5 6 7
-// . . . . . . . . . . . . + + + + + + + + + + + + + . . . . . . . . . . . . . - - ||- - - - - - - - - - -
-// 11 cut off, last 11
-
-// 8 % 12 = 4 -> 12 + 4
-func (this *GameMap) FFT2(strNum string) string {
-	this.numstr = "12345678"
-	this.numLength = len(this.numstr)
-	var numStr string
-	var val, remainVal int
-	fullPhase := this.getFullPhaseVal(strNum)
-	newLength := this.numLength * 5
-	str := strings.Repeat(strNum, this.numLength)
-
-	for i := 0; i < newLength; i++ {
-		// if i%1000 == 0 {
-		// 	fmt.Println(i, newLength)
-		// }
-		length := (4 * (i + 1))
-		phaseVal := fullPhase[i%this.numLength]
-
-		times := newLength / length
-		remain := newLength % length
-		if remain != 0 {
-			remainVal = this.getPhaseVal(i, str[:remain])
-		}
-		val = phaseVal*times + remainVal
-		if val < 0 {
-			val *= -1
-		}
-		// fmt.Println("\nnum", i, "val", val, remainVal, newLength, length, remain, length-remain, str[:remain])
-		// fmt.Println("calcs", this.numLength/length, this.numLength%length)
-		// fmt.Println("calc2", length/this.numLength, length%this.numLength)
-
-		lastDigit := val % 10
-		numStr += strconv.Itoa(lastDigit)
+func (this *FFT) FFT2(intList []int) []int {
+	var newList []int
+	var sumVal int
+	for _, val := range intList {
+		sumVal += val
 	}
-	return numStr
+	for _, val := range intList {
+		newList = append(newList, sumVal%10)
+		sumVal -= val
+	}
+	return newList
 }
+func (this *FFT) runHack() string {
+	var intList []int
+	var strList []string
+	offset, _ := strconv.Atoi(this.numstr[:7])
+	newStr := strings.Repeat(this.numstr, 10_000)[offset:]
 
-func (this *GameMap) runFFT2() string {
-	repeatedStr := strings.Repeat(this.numstr, 5)
-	var i int
-	for i < 1 {
-		repeatedStr = this.FFT2(repeatedStr)
-		i += 1
+	for _, char := range newStr {
+		num, _ := strconv.Atoi(string(char))
+		intList = append(intList, num)
 	}
 
-	return repeatedStr
+	for i := 0; i < 100; i++ {
+		// Trick* Second half of output correspons with sum of second half of input
+		// The offset only occurs on the second half
+		intList = this.FFT2(intList)
+	}
+	for _, num := range intList[:8] {
+		strList = append(strList, strconv.Itoa(num))
+	}
+
+	return strings.Join(strList, "")
 }
+
 func main() {
-	gameMap := &GameMap{
-		UseTest: true,
+	FFT := &FFT{
+		UseTest: false,
 	}
-	gameMap.getInput()
-	fmt.Println("Day 16 part 1:", gameMap.runFFT())
-	fmt.Println("Day 16 part 2:", gameMap.runFFT2())
+	FFT.getInput()
+	fmt.Println("Day 16 part 1:", FFT.run())
+	fmt.Println("Day 16 part 2:", FFT.runHack())
 }
-
-//00524718
