c514f0304a10a856f4503d58d8bb81f495cec7f7
fchoi1 <fchoi@outlook.com>
2024-12-02 01:12:53 -0500

    day 2 of 204

--- 2024/day2/day2.js
+++ 2024/day2/day2.js
@@ -6,7 +6,7 @@ class Day2 {
     try {
       const data = fs.readFileSync(inputFile, "utf8").trim().split(/\r?\n/);
       return data.map((dimension) => {
-        return dimension.split("x").map((str) => parseInt(str));
+        return dimension.split(" ").map((str) => parseInt(str));
       });
     } catch (err) {
       throw err;
@@ -15,14 +15,50 @@ class Day2 {
 
   constructor(useTest = false) {
     this.useTest = useTest;
-    this.gifts = this.getInput();
+    this.reports = this.getInput();
   }
 
-  getArea() {
-    return 1;
+  isValid(report) {
+    let prev;
+    const inc = report[1] > report[0];
+    for (let val of report) {
+      if (!prev) {
+        prev = val;
+        continue;
+      }
+
+      const isUnsafe =
+        Math.abs(val - prev) > 3 ||
+        val == prev ||
+        (inc && val < prev) ||
+        (!inc && val > prev);
+
+      if (isUnsafe) return false;
+      prev = val;
+    }
+    return true;
+  }
+
+  isValid2(report) {
+    for (let i = 0; i < report.length; i++) {
+      const removed = report.splice(i, 1);
+
+      if (this.isValid(report)) return true;
+      report.splice(i, 0, ...removed);
+    }
+  }
+
+  getArea(isPart2 = false) {
+    let count = 0;
+    this.reports.forEach((report) => {
+      if (isPart2 && (this.isValid(report) || this.isValid2(report))) count++;
+      else if (this.isValid(report)) count++;
+    });
+
+    return count;
   }
 }
 
 const day2 = new Day2();
 console.log("Day 2 part 1:", day2.getArea());
-// console.log("Day 2 part 2:", day2.getRibbonLength());
+console.log("Day 2 part 2:", day2.getArea(true));
