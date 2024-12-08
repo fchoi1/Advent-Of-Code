d84eb12d4bd4cf7968ab856685d712d0b08dca54
fchoi1 <fchoi@outlook.com>
2024-12-07 19:54:28 -0500

    done day 25

---
+++ 2024/day7/day7.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+class BridgeRepair {
+  getInput() {
+    const inputFile = this.useTest ? "input-test.txt" : "input.txt";
+    try {
+      const data = fs.readFileSync(inputFile, "utf8").trim().split(/\r?\n/);
+      return data.map((dimension) => {
+        const split = dimension.split(": ");
+        return {
+          answer: parseInt(split[0]),
+          numbers: split[1].split(" ").map((x) => parseInt(x)),
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  constructor(useTest = false) {
+    this.useTest = useTest;
+    this.equations = this.getInput();
+  }
+
+  isValid(ans, numbers, curr, index, isPart2 = false) {
+    if (ans === curr) return true;
+    if (curr > ans || index >= numbers.length) return false;
+    const mul = this.isValid(
+      ans,
+      numbers,
+      curr * numbers[index],
+      index + 1,
+      isPart2
+    );
+    const add = this.isValid(
+      ans,
+      numbers,
+      curr + numbers[index],
+      index + 1,
+      isPart2
+    );
+
+    const concat = isPart2
+      ? this.isValid(
+          ans,
+          numbers,
+          parseInt(String(curr) + String(numbers[index])),
+          index + 1,
+          isPart2
+        )
+      : false;
+    return mul || add || concat;
+  }
+
+  countValid(isPart2 = false) {
+    let count = 0;
+    this.equations.forEach(({ answer, numbers }) => {
+      if (this.isValid(answer, numbers, numbers[0], 1, isPart2))
+        count += answer;
+    });
+    return count;
+  }
+}
+
+const bridgeRepair = new BridgeRepair();
+console.log("Day 7 part 1:", bridgeRepair.countValid());
+console.log("Day 7 part 2:", bridgeRepair.countValid(true));
