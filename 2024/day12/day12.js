644e24781eda99d1d6f4d41cf18d21738e8e43bb
fchoi1 <fchoi@outlook.com>
2024-12-15 18:40:50 -0500

    done part 2 of day 12

--- 2024/day12/day12.js
+++ 2024/day12/day12.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-class Pebbles {
+class GardenGroups {
   getInput() {
     const inputFile = this.useTest ? "input-test.txt" : "input.txt";
     try {
@@ -14,6 +14,8 @@ class Pebbles {
   constructor(useTest = false) {
     this.useTest = useTest;
     this.grid = this.getInput();
+    this.part1 = 0;
+    this.part2 = 0;
     this.w = this.grid[0].length;
     this.h = this.grid.length;
     this.directions = [
@@ -22,9 +24,10 @@ class Pebbles {
       [-1, 0], //left
       [0, -1], // up
     ];
+    this.regions = this.getRegions();
   }
 
-  getPrice(x, y, seen, area, isPart2 = false) {
+  processRegion(x, y, seen, area) {
     let perimeter = 1;
     let region = new Set();
 
@@ -54,108 +57,69 @@ class Pebbles {
         q.push({ x: x + dx, y: y + dy });
       }
     }
-    if (!isPart2) return perimeter * region.size;
-
-    let edges = new Set();
-    let isEdge = false;
-    let start;
-
-    for (const coord of region) {
-      const [x, y] = coord.split(",").map(Number);
-      for (const [dx, dy] of this.directions) {
-        if (!region.has(`${x + dx},${y + dy}`)) {
-          isEdge = true;
-          break;
-        }
-      }
-      if (isEdge) {
-        edges.add(coord);
-        if (
-          !start &&
-          !region.has(`${x - 1},${y}`) &&
-          !region.has(`${x},${y - 1}`)
-        )
-          start = [x, y];
-      }
-    }
-
-    let [currX, currY] = start;
-    const seenEdges = new Set();
-    let dir = 0;
-    let startkey = `${x},${y},${this.directions[dir][0]},${this.directions[dir][1]}`;
-    let key;
-    let sides = 0;
-    console.log("LETTER", area);
-    let count = 0;
-    while (startkey !== key) {
-      count += 1;
-      console.log("key", key, "start", startkey, area);
-
-      const lx = this.directions[(dir + 4 - 1) % 4][0];
-      const ly = this.directions[(dir + 4 - 1) % 4][1];
-      while (
-        edges.has(`${currX},${currY}`) &&
-        !edges.has(`${currX + lx},${currY + ly}`)
-      ) {
-        seenEdges.add(`${currX},${currY}`);
-        console.log("Adding", `${currX},${currY}`);
-        currX += this.directions[dir][0];
-        currY += this.directions[dir][1];
-      }
-      currX -= this.directions[dir][0];
-      currY -= this.directions[dir][1];
-      console.log("curr", currX, currY, this.directions[dir]);
-
-
-
-      if (
-        edges.has(`${currX + lx},${currY + ly}`) &&
-        !seenEdges.has(`${currX + lx},${currY + ly}`)
-      ) {
-        dir = (dir + 4 - 1) % 4;
-        console.log("TURN LEFT");
-      } else {
-        dir = (dir + 1) % 4;
-        console.log("TURN RIGHT");
-      }
-
-      // if (
-      //   edges.has(
-      //     `${currX + this.directions[dir][0]},${
-      //       currY + this.directions[dir][1]
-      //     }`
-      //   )
-      // ) {
-      //   currX += this.directions[dir][0];
-      //   currY += this.directions[dir][1];
-      // }
-
-      sides += 1;
-      key = `${currX},${currY},${this.directions[dir][0]},${this.directions[dir][1]}`;
-      // console.log(dir, "New", currX, currY, sides);
-      // console.log(key, seenEdges);
-    }
-
-    console.log(sides, perimeter, area);
-    return sides * region.size;
+    return [perimeter * region.size, region];
   }
 
-  countStones(isPart2 = false) {
+  getRegions() {
     let cost = 0;
+    const regions = [];
     const seen = new Set();
     for (let j = 0; j < this.h; j++) {
       for (let i = 0; i < this.w; i++) {
         const key = `${i},${j}`;
         if (seen.has(key)) continue;
-        const c = this.getPrice(i, j, seen, this.grid[j][i], isPart2);
-
+        const [c, r] = this.processRegion(i, j, seen, this.grid[j][i]);
+        regions.push(r);
         cost += c;
       }
+    }
+    this.part1 = cost;
+    return regions;
+  }
+
+  getSides(region) {
+    let sides = 0;
+
+    const hd = [
+      [0.5, 0.5],
+      [-0.5, 0.5],
+      [-0.5, -0.5],
+      [0.5, -0.5],
+    ];
+
+    const corners = new Set();
+    for (const coord of region) {
+      const [x, y] = coord.split(",").map(Number);
+      hd.forEach(([dx, dy]) => corners.add(`${x + dx},${y + dy}`));
+    }
+
+    for (const coords of corners) {
+      const [cx, cy] = coords.split(",").map(Number);
+      const config = hd.map(([dx, dy]) => {
+        return region.has(`${cx + dx},${cy + dy}`);
+      });
+
+      const s = config.reduce((acc, val) => acc + val, 0);
+      if (s === 3 || s === 1) sides += 1;
+      if (s === 2) {
+        if ((config[0] && config[2]) || (config[1] && config[3])) sides += 2;
+      }
+    }
+
+    return sides * region.size;
+  }
+
+  getPrice(isPart2 = false) {
+    if (!isPart2) return this.part1;
+
+    let cost = 0;
+    for (const region of this.regions) {
+      cost += this.getSides(region);
     }
     return cost;
   }
 }
 
-const pebbles = new Pebbles(true);
-console.log("Day 12 part 1:", pebbles.countStones(true));
-// console.log("Day 11 part 2:", pebbles.countStones(true));
+const gardenGroups = new GardenGroups();
+console.log("Day 12 part 1:", gardenGroups.getPrice());
+console.log("Day 12 part 2:", gardenGroups.getPrice(true));
