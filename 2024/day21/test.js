064fd4568453b7ece85b07ae071849941ddf63c1
fchoi1 <fchoi@outlook.com>
2024-12-28 10:00:00 +0000

    removed test files

--- 2024/day21/test.js
+++
@@ -1,3 +0,0 @@
-// <vA<AA>>^AvAA<^A>Av<<A>>^AvA^A<vA>^Av<<A>^A>AAvA^Av<<A>A>^AAAvA<^A>A
-// <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-// <vA<AA>>^AvAA<^A>Av<<A>>^AvA^A<vA>^Av<<A>^A>AAvA^Av<<A>A>^AAAvA<^A>A
