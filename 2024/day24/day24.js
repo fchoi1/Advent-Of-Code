c6612353401585a805d9d9c7971ff2400bed55cd
fchoi1 <fchoi@outlook.com>
2025-02-25 23:32:42 -0500

    done manually for day24

--- 2024/day24/day24.js
+++ 2024/day24/day24.js
@@ -37,8 +37,13 @@ class MonkeyMarket {
       XOR: (a, b) => a ^ b,
     };
     this.parseEdges();
-    console.log(this.startValues, this.outputs);
-    console.log("vars", this.vars);
+    this.cache = {};
+    // console.log(this.values);
+    // console.log("vars", this.vars);
+  }
+
+  makeKey(key, num) {
+    return key + num.toString().padStart(2, "0");
   }
 
   parseEdges() {
@@ -58,28 +63,144 @@ class MonkeyMarket {
 
   getZ() {
     let ans = "";
-    let key = "z00";
+    let num = 0;
+    let key = this.makeKey("z", num);
     while (this.vars.has(key)) {
-      const val = this.calculateValue(key);
-      ans = String(val) + ans;
-      key = `z${(+key.slice(1) + 1).toString().padStart(2, "0")}`;
+      ans = String(this.calculateValue(key)) + ans;
+      num += 1;
+      key = this.makeKey("z", num);
     }
-
-    console.log(ans, ans.length);
-    console.log("0011111101000".length)
-
-    return parseInt(ans, 2);
+    return ans;
   }
 
+  getDecimal() {
+    return parseInt(this.getZ(), 2);
+  }
 
-  // function to suppy inputs and get output
-  // if we get a wrong answer proprate from output and check 
-  // ex output was 0 but should be 1
-  // a and b = 0
-  // that means a must be 1 and b must be 1
-  // other way arround is that a =1 ,b = 0, or b = 0, a = 1  or a = 0, b = 0
-  // one of them is incorrct?
+  printWire(wire, depth) {
+    if (wire.startsWith("x") || wire.startsWith("y"))
+      return `${" ".repeat(depth)}${wire}`;
+
+    const { a, b, cmd } = this.vars.get(wire);
+    const line = `${" ".repeat(depth)}${wire} ${cmd}\n${this.printWire(
+      a,
+      depth + 1
+    )}\n${this.printWire(b, depth + 1)}`;
+    return line;
+  }
+
+  validateProgram() {
+    for (let i = 0; i < 45; i++) {
+      if (!this.verify(i)) {
+        console.log("Failed", i);
+        return i;
+      }
+    }
+    return 45;
+  }
+
+  part2() {
+    console.log("\n\n");
+    console.log(this.printWire("z02", 0));
+
+    const swap = [];
+
+    while (!this.validateProgram()) {}
+
+    // check swaps
+
+    // console.log(this.cache);
+
+    // Swap
+    // hbs <> kfp
+    // dhq <> z18
+    // z22 <> pdg
+    // z27 <> jcp
+
+    // const swap = ["hbs", "kfp", "dhq", "z18", "z22", "pdg", "z27", "jcp"];
+    return swap.sort().join(",");
+  }
+
+  verifyImmeditate(key, num) {
+    const { a, b, cmd } = this.vars.get(key);
+    console.log("verifyImmeditate", key);
+
+    if (cmd != "XOR") return false;
+
+    const x = this.makeKey("x", num);
+    const y = this.makeKey("y", num);
+    return (a === x && b === y) || b === x || a === y;
+  }
+
+  verifyPrevCarry(key, num) {
+    console.log("verifyPrevCarry", key);
+    const { a, b, cmd } = this.vars.get(key);
+
+    if (cmd != "AND") return false;
+    const x = this.makeKey("x", num - 1);
+    const y = this.makeKey("y", num - 1);
+    return (a === x && b === y) || b === x || a === y;
+  }
+
+  verifyReCarry(key, num) {
+    console.log("verifyReCarry", key);
+    if (this.cache[key]) return true;
+
+    const { a, b, cmd } = this.vars.get(key);
+    if (cmd != "AND") return false;
+    const valid =
+      (this.verifyImmeditate(a, num - 1) && this.verifyCarry(b, num - 1)) ||
+      (this.verifyImmeditate(b, num - 1) && this.verifyCarry(a, num - 1));
+
+    if (valid) this.cache[key] = true;
+
+    return valid;
+  }
+
+  verifyCarry(key, num) {
+    console.log("verifyCarry", key);
+    const { a, b, cmd } = this.vars.get(key);
+
+    if (this.cache[key]) return true;
+
+    if (cmd != "OR" && num !== 1) return false;
+
+    const valid =
+      num === 1
+        ? this.verifyPrevCarry(key, num)
+        : (this.verifyPrevCarry(a, num) && this.verifyReCarry(b, num)) ||
+          (this.verifyPrevCarry(b, num) && this.verifyReCarry(a, num));
+
+    if (valid) this.cache[key] = true;
+
+    return valid;
+  }
+
+  verify(num) {
+    const key = this.makeKey("z", num);
+    console.log("\nVerify", key);
+    if (key === "z00") return this.verifyImmeditate(key, 0);
+
+    const { a, b, cmd } = this.vars.get(key);
+    if (cmd != "XOR") return false;
+
+    return (
+      (this.verifyImmeditate(a, num) && this.verifyCarry(b, num)) ||
+      (this.verifyImmeditate(b, num) && this.verifyCarry(a, num))
+    );
+  }
+
+  // bit addition
+  // First
+  // carry = x and y
+  // val = x xor y
+
+  // Second
+  // carry = (x and y) or (prev carry and (prev x xor  prev y))
+  // val  = (x xor y) xor (prev carry and (prev x xor  prev y)) or (prev x and prev y))
+  //      (either one)   xor  ( carry and only 1   or   both )
 }
 
 const monkeyMarket = new MonkeyMarket();
-console.log("Day 24 part 1:", monkeyMarket.getZ());
+console.log("Day 24 part 1:", monkeyMarket.getDecimal());
+console.log("Day 24 part 2:", monkeyMarket.part2());
