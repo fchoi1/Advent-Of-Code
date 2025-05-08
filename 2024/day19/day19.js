24be64270bfe0cb279b29cd964438f121e8280ae
fchoi1 <fchoi@outlook.com>
2025-05-08 00:00:43 -0400

    done part 2 day 19

--- 2024/day19/day19.js
+++ 2024/day19/day19.js
@@ -25,34 +25,15 @@ class RunRAM {
   constructor(useTest = false) {
     this.useTest = useTest;
     [this.patterns, this.designs] = this.getInput();
-    this.getLongest();
-    this.cache = new Map();
-    console.log(this.longest, this.patterns.size, this.designs.length);
-    this.filterPatterns();
-    // console.log(this.cache);
-    console.log(this.patterns.size, this.designs.length);
+    this.longest = this.getLongest();
   }
 
   getLongest() {
-    this.longest = 0;
+    let longest = 0;
     for (let p of this.patterns) {
-      if (p.length > this.longest) this.longest = p.length;
+      if (p.length > longest) longest = p.length;
     }
-  }
-
-  filterPatterns() {
-    const unqiue = new Set();
-    const list = [...this.patterns];
-    for (let p of list) {
-      this.patterns.delete(p);
-      if (!this.bfs(p)) {
-        this.cache.set(p, 1);
-      } else {
-        this.cache.set(p, this.bfs2(p) + 1);
-      }
-      this.patterns.add(p);
-    }
-    // this.patterns = unqiue;
+    return longest;
   }
 
   bfs(string) {
@@ -77,129 +58,51 @@ class RunRAM {
     return false;
   }
 
-  bfs2(string) {
-    const q = [string];
-    const seen = new Set();
-    let c = 0;
-
-    while (q.length > 0) {
-      // q.sort((a, b) => a.length - b.length);
-      const str = q.shift();
-      if (this.patterns.has(str)) {
-        c += 1;
-      }
-
-      if (!str || str.length === 0) continue;
-      if (seen.has(str)) continue;
-      // seen.add(str);
-
-      for (let i = 0; i <= this.longest; i++) {
-        if (this.patterns.has(str.slice(0, i))) {
-          if (str.slice(i).length === 0) continue;
-          q.push(str.slice(i));
-        }
-      }
-    }
-    return c;
-  }
-
-  bfs3(string) {
-    const q = [[string, 1]];
-    const seen = new Set();
-    let c = 0;
-
-    while (q.length > 0) {
-      // q.sort((a, b) => a.length - b.length);
-      const [str, count] = q.shift();
-
-      if (!str || str.length === 0) continue;
-      if (seen.has(str)) continue;
-      seen.add(str);
-
-      if (this.patterns.has(str)) {
-        // console.log("found a combo:", count, str);
-        c += count;
-      }
-
-      for (let i = 0; i <= this.longest; i++) {
-        if (this.patterns.has(str.slice(0, i))) {
-          if (str.slice(i).length === 0) continue;
-          // console.log(
-          //   "found",
-          //   str.slice(0, i),
-          //   count * this.cache.get(str.slice(0, i))
-          // );
-          q.push([str.slice(i), count * this.cache.get(str.slice(0, i))]);
-        }
-      }
-    }
-    return c;
-  }
-
-  checkDesign(design) {
-    // console.log(design);
-    let c = 0;
-    // console.log("design", design);
-    if (this.cache.has(design)) {
-      // console.log("cached", design, this.cache.get(design));
-      return this.cache.get(design);
-    }
-    if (this.patterns.has(design)) {
-      this.cache.set(design, 1);
-      // console.log("seeting cache", design, 1, this.cache);
-      c += 1;
-    }
-
-    for (let i = 0; i <= this.longest; i++) {
-      if (this.patterns.has(design.slice(0, i))) {
-        // console.log("Checking", design, "sliced", design.slice(i));
-        if (design.slice(i).length === 0) continue;
-        c += this.checkDesign(design.slice(i));
-      }
-    }
-    if (c !== 0) {
-      // console.log("set here c", design, c);
-      this.cache.set(design, c);
-    }
-    // console.log("design", design, c);
-    return c;
-  }
-
-  part1() {
+  countValidDesigns() {
     let ans = 0;
-    // console.log(
-    //   this.checkDesign(
-    //     "brbbrrugubbbuwbrrwbwwggwrgbgbbbwrbbubbrggruggbgrgubgbbwgbubg"
-    //   )
-    // );
     for (let design of this.designs) {
-      console.log(design);
-
-      if (this.bfs(design)) {
-        ans += 1;
-      }
+      if (this.bfs(design)) ans += 1;
     }
     return ans;
   }
 
-  part2() {
-    let c = [];
+  subtractSuffix(str1, str2) {
+    return str1.endsWith(str2) ? str1.slice(0, -str2.length) : str1;
+  }
 
-    for (let design of this.designs) {
-      // this.cache = new Map();
-      console.log(design);
-      const counts = this.bfs3(design);
-      if (counts > 0) {
-        // console.log("\n\ncount", counts, design);
-        // console.log("\n\n");
-        c.push(counts);
+  getCounts(design) {
+    let dp = new Map(); // string: count
+    let s = "";
+
+    for (let i = 0; i < design.length; i++) {
+      let currCount = 0;
+      s += design[i];
+
+      let subString = "";
+      for (let j = i; j >= 0; j--) {
+        subString = design[j] + subString;
+        if (subString.length > this.longest) break;
+
+        if (this.patterns.has(subString)) {
+          const subtracted = this.subtractSuffix(s, subString);
+          if (subtracted === "") currCount += 1;
+          else currCount += dp.get(subtracted, 0);
+        }
       }
+      dp.set(s, currCount);
     }
-    console.log(c);
-    return c.reduce((a, b) => a + b);
+    return dp.get(design);
+  }
+
+  getAllCombos() {
+    let total = 0;
+    for (let design of this.designs) {
+      total += this.getCounts(design);
+    }
+    return total;
   }
 }
 
 const runRAM = new RunRAM();
-// console.log("Day 19 part 1:", runRAM.part1());
-console.log("Day 19 part 2:", runRAM.part2());
+console.log("Day 19 part 1:", runRAM.countValidDesigns());
+console.log("Day 19 part 2:", runRAM.getAllCombos());
