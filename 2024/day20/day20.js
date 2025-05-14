8fde3518afc76dbd83222f6abd8a9a1cf5a8faa0
fchoi1 <fchoi@outlook.com>
2025-05-13 22:11:43 -0400

    done day 20 part 2

--- 2024/day20/day20.js
+++ 2024/day20/day20.js
@@ -15,8 +15,7 @@ class RunRAM {
     this.useTest = useTest;
     this.grid = this.getInput();
 
-    [this.start, this.end] = this.getLoc(this.grid);
-    this.save = useTest ? 2 : 100;
+    [this.start, this.end] = this.getLoc();
     this.h = this.grid.length;
     this.w = this.grid[0].length;
     this.dir = [
@@ -25,137 +24,109 @@ class RunRAM {
       [0, -1],
       [-1, 0],
     ];
-
-    console.log(this.start, this.end);
-    console.log("Dimensions:", this.h, this.w);
+    this.save = useTest ? 2 : 100;
+    this.costMap = this.fillGrid();
+    this.totalSteps = this.costMap.get(`${this.end[0]},${this.end[1]}`);
   }
 
-  printMap(map) {
-    for (const row of map) {
-      console.log(row.join(""));
-    }
-  }
-
-  getLoc(map) {
+  getLoc() {
     let start, end;
-    for (let j = 0; j < map.length; j++) {
-      for (let i = 0; i < map[0].length; i++) {
-        if (map[j][i] === "S") start = [i, j];
-        if (map[j][i] === "E") end = [i, j];
+    for (let j = 0; j < this.grid.length; j++) {
+      for (let i = 0; i < this.grid[0].length; i++) {
+        if (this.grid[j][i] === "S") start = [i, j];
+        if (this.grid[j][i] === "E") end = [i, j];
       }
     }
     return [start, end];
   }
 
-  getPath(start, end, map) {
-    const q = [[start, 0]];
+  findValid(start, range) {
+    const q = [[start, 0]]; // coord, steps
     const visited = new Set();
+    const validTrack = new Map();
     while (q.length) {
       const [curr, steps] = q.shift();
       const [x, y] = curr;
-      if (x === end[0] && y === end[1]) return steps;
+      if (steps > range) continue;
+      if ((x != start[0] || y != start[1]) && this.grid[y][x] !== "#") {
+        if (
+          this.costMap.get(`${x},${y}`) >
+          this.costMap.get(`${start[0]},${start[1]}`)
+        ) {
+          validTrack.set(`${x},${y}`, steps);
+        }
+      }
 
       for (const [dx, dy] of this.dir) {
-        const nextX = x + dx;
-        const nextY = y + dy;
-        if (nextX < 0 || nextX >= this.w || nextY < 0 || nextY >= this.h)
-          continue;
-        if (map[nextY][nextX] === "#") continue;
-        if (visited.has(`${nextX},${nextY}`)) continue;
-        visited.add(`${nextX},${nextY}`);
-        q.push([[nextX, nextY], steps + 1]);
+        const nx = x + dx;
+        const ny = y + dy;
+        if (nx < 0 || nx >= this.w || ny < 0 || ny >= this.h) continue;
+        if (visited.has(`${nx},${ny}`)) continue;
+        visited.add(`${nx},${ny}`);
+        q.push([[nx, ny], steps + 1]);
       }
     }
+    return validTrack;
   }
 
-  fillGrid(start, map) {
+  fillGrid() {
     const costMap = new Map();
-    const q = [[start, 0]];
-    const visited = new Set([`${start[0]},${start[1]}`]);
+    const q = [[this.start, 0]];
+    const visited = new Set([`${this.start[0]},${this.start[1]}`]);
+
     while (q.length) {
       const [curr, steps] = q.shift();
       const [x, y] = curr;
       costMap.set(`${x},${y}`, steps);
 
       for (const [dx, dy] of this.dir) {
-        const nextX = x + dx;
-        const nextY = y + dy;
-        if (nextX < 0 || nextX >= this.w || nextY < 0 || nextY >= this.h)
-          continue;
-        if (map[nextY][nextX] === "#") continue;
-        if (visited.has(`${nextX},${nextY}`)) continue;
-        visited.add(`${nextX},${nextY}`);
-        q.push([[nextX, nextY], steps + 1]);
+        const nx = x + dx;
+        const ny = y + dy;
+        if (nx < 0 || nx >= this.w || ny < 0 || ny >= this.h) continue;
+        if (this.grid[ny][nx] === "#") continue;
+        if (visited.has(`${nx},${ny}`)) continue;
+        visited.add(`${nx},${ny}`);
+        q.push([[nx, ny], steps + 1]);
       }
     }
-    console.log(costMap);
     return costMap;
   }
 
-  getSteps() {
-    const steps = this.bfs(this.start, this.end, this.grid);
-    console.log("total steps", steps);
+  getSteps(isPart2 = false) {
     const save = {};
+    const range = isPart2 ? 20 : 2;
+    if (isPart2) this.save = this.useTest ? 50 : 100;
 
     let count = 0;
 
     for (let j = 1; j < this.h - 1; j++) {
       for (let i = 1; i < this.w - 1; i++) {
-        if (this.grid[j][i] === "E" || this.grid[j][i] === "S") continue;
-        if (this.grid[j][i] === ".") continue;
-        // right
-        let test;
+        if (this.grid[j][i] === "E" || this.grid[j][i] === "#") continue;
 
-        for (const [dx, dy] of this.dir) {
-          const nextX = x + dx;
-          const nextY = y + dy;
-          if (nextX < 0 || nextX >= this.w || nextY < 0 || nextY >= this.h)
-            continue;
-          if (this.grid[nextY][nextX] === "#" || nextX == i  ) continue;
-        }
+        const validTrack = this.findValid([i, j], range);
 
-      
+        for (const [key, steps] of validTrack) {
+          const [x, y] = key.split(",").map((x) => parseInt(x));
 
-        this.grid[j][i] = ".";
-        // this.grid[j][i + 1] = "X";
+          const startSteps = this.costMap.get(`${i},${j}`);
+          const endSteps = this.costMap.get(`${x},${y}`);
+          const tripSteps = startSteps + steps + this.totalSteps - endSteps;
+          const diff = this.totalSteps - tripSteps;
 
-        test = this.bfs(this.start, this.end, this.grid);
-        const diff = steps - test;
-        if (diff >= this.save) {
-          // this.printMap(this.grid);
-          if (!save[diff]) save[diff] = 1;
-          else save[diff]++;
-          count += 1;
-
-          // console.log("stesp", test, "Diff", steps - test);
-        }
-  
-      }
-    }
-    // console.log(save);
-    return count;
-  }
-  getByte() {
-    const costMap = this.fillGrid(this.end, this.grid);
-
-    const cellWidth = 5; // Adjust width based on your needs
-    for (let j = 0; j < this.h; j++) {
-      let str = "";
-      for (let i = 0; i < this.w; i++) {
-        const key = `${i},${j}`;
-        if (costMap.has(key)) {
-          str += costMap.get(key).toString().padStart(cellWidth, " ");
-        } else {
-          str += this.grid[j][i].toString().padStart(cellWidth, " ");
+          if (diff >= this.save) {
+            if (!save[diff]) save[diff] = 1;
+            else save[diff]++;
+            count += 1;
+          }
         }
       }
-      console.log(str);
     }
 
-    return this.part2;
+    return Object.values(save).reduce((a, b) => a + b, 0);
   }
 }
 
-const runRAM = new RunRAM(true);
-// console.log("Day 20 part 1:", runRAM.getSteps());
-console.log("Day 20 part 2:", runRAM.getByte());
+const runRAM = new RunRAM();
+console.log("Day 20 part 1:", runRAM.getSteps());
+console.log("Day 20 part 2:", runRAM.getSteps(true));
+// Total Runtime ~3.4s
